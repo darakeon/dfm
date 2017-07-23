c95f86d54926e252677aec5689b5271ea92df9e8
Dara Keon <lc.sales.duarte@gmail.com>
2017-07-23 02:31:41 -0300

    Created console for tests
    by [Dara Keon (Ísis)] in [2013-03-07 16:59:48 (-03:00)] - hg hash {36b35f00e4f6}

---
+++ DFM.Console4Tests/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+using System.Linq;
+
+namespace DFM.Console4Tests
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var path = @"Tasks.txt";
+
+            var lines = File.ReadLines(path);
+
+            var newLines = lines.Reverse().ToList();
+
+            File.WriteAllLines(path, newLines);
+        }
+    }
+}
