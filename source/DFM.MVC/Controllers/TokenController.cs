852e1a9c694dd93df7bba66dce8fa1e2ffa40ae2
Dara Keon <lc.sales.duarte@gmail.com>
2017-08-12 21:17:01 -0300

    Fixed activate user
    by [Dara Keon (Ísis)] in [2013-08-14 21:21:39 (-03:00)] - hg hash {852b6d6e50d8}

--- source/DFM.MVC/Controllers/TokenController.cs
+++ source/DFM.MVC/Controllers/TokenController.cs
@@ -116,7 +116,7 @@ namespace DFM.MVC.Controllers
                 return invalidTokenAction();
             }
 
-            //Services.Safe.ActivateUser(id);
+            Services.Safe.ActivateUser(id);
 
             return View("UserVerificationSuccess");
         }
