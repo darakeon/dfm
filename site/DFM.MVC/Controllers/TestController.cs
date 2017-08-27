c507aa2ae520848665713dee8d5355df45e27b1e
Dara Keon <lc.sales.duarte@gmail.com>
2017-08-27 04:30:10 -0300

    Authorize with admin
    by [Dara Keon (Ísis)] in [2014-06-20 02:24:33 (-03:00)] - hg hash {aa2fdbeeb27d}

--- site/DFM.MVC/Controllers/TestController.cs
+++ site/DFM.MVC/Controllers/TestController.cs
@@ -1,8 +1,10 @@
 ﻿using System.Web.Mvc;
 using DFM.Multilanguage;
+using DFM.MVC.Helpers.Authorize;
 
 namespace DFM.MVC.Controllers
 {
+    [DFMAuthorize(true)]
     public class TestController : Controller
     {
         public ActionResult Index()
--- site/DFM.MVC/Helpers/Authorize/DFMAuthorizeAttribute.cs
+++ site/DFM.MVC/Helpers/Authorize/DFMAuthorizeAttribute.cs
@@ -1,13 +1,22 @@
-﻿using System.Web;
+﻿using System;
+using System.Web;
 using System.Web.Mvc;
 
 namespace DFM.MVC.Helpers.Authorize
 {
     public class DFMAuthorizeAttribute : AuthorizeAttribute
     {
+        private readonly Boolean admin;
+
+        public DFMAuthorizeAttribute(Boolean admin = false)
+        {
+            this.admin = admin;
+        }
+
         protected override bool AuthorizeCore(HttpContextBase httpContext)
         {
-            return Auth.Current.IsAuthenticated;
+            return Auth.Current.IsAuthenticated
+                && (!admin || Auth.Current.IsAdm);
         }
     }
 }
