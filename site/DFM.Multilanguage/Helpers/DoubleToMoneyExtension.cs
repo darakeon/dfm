59c53ec0e67e8048300520bc3d854ce047dc9542
Dara Keon <lc.sales.duarte@gmail.com>
2017-08-27 00:14:31 -0300

    Put point thousand in visible numbers; fixed reference to ToMoney
    by [Dara Keon (desktop)] in [2013-09-26 23:04:19 (-03:00)] - hg hash {3959c2b5074e}

--- site/DFM.Multilanguage/Helpers/DoubleToMoneyExtension.cs
+++ site/DFM.Multilanguage/Helpers/DoubleToMoneyExtension.cs
@@ -6,7 +6,7 @@ namespace DFM.Multilanguage.Helpers
     {
         public static String ToMoney(this Double number, String language)
         {
-            return number.ToString("#0.00", PlainText.GetNumberFormat(language));
+            return number.ToString("#,###0.00", PlainText.GetNumberFormat(language));
         }
     }
 }
