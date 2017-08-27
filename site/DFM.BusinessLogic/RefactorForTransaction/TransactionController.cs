8e425f2f1e73e5ef08600357bdcc61c8fada5047
Dara Keon <lc.sales.duarte@gmail.com>
2017-08-27 04:46:52 -0300

    Code to refactor TransactionController
    by [Dara Keon (Ísis)] in [2014-07-23 15:34:40 (-03:00)] - hg hash {8574185dd7a8}

---
+++ site/DFM.BusinessLogic/RefactorForTransaction/TransactionController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Test
+{
+    internal class Base
+    {
+        protected T SafeExecutor<T>(Func<T> action)
+        {
+            Console.WriteLine("...");
+
+            try
+            {
+                var result = action();
+
+                Console.WriteLine("=)");
+
+                return result;
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Oh shit!");
+                throw;
+            }
+        }
+
+        protected void SafeExecutor(Action action)
+        {
+            Console.WriteLine("...");
+
+            try
+            {
+                action();
+
+                Console.WriteLine("=)");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Oh shit!");
+                throw;
+            }
+        }
+
+    }
+}
