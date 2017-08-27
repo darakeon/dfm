508a8733754dfd8b1c313bcf522fd921ad2dd77f
Dara Keon <lc.sales.duarte@gmail.com>
2017-08-27 03:50:54 -0300

    Made zero values blue at site
    by [Dara Keon (Ísis)] in [2014-04-18 05:27:03 (-03:00)] - hg hash {7b94d460013b}

---
+++ site/DFM.MVC/Helpers/Views/ValueSign.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace DFM.MVC.Helpers.Views
+{
+    //TODO: REMOVE THIS SHIT
+    public static class ValueSign
+    {
+        public static Boolean IsNegative(this Double number)
+        {
+            return (Int32)(number * 100) < 0;
+        }
+
+    }
+}
