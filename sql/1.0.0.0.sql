bcad114084f605f5fc2fe05555541ed0b960391a
Dara Keon <lc.sales.duarte@gmail.com>
2017-08-27 02:49:27 -0300

    Adjusting spaces
    by [Dara Keon (desktop)] in [2014-03-04 03:27:11 (-03:00)] - hg hash {7bbf5966d321}

--- sql/1.0.0.0.sql
+++ sql/1.0.0.0.sql
@@ -1,87 +1,87 @@
     create table Account (
         ID INTEGER NOT NULL AUTO_INCREMENT,
-       Name VARCHAR(20) not null,
-       EndDate DATETIME,
-       RedLimit DOUBLE,
-       YellowLimit DOUBLE,
-       BeginDate DATETIME not null,
-       User_ID INTEGER not null,
-       primary key (ID)
+        Name VARCHAR(20) not null,
+        EndDate DATETIME,
+        RedLimit DOUBLE,
+        YellowLimit DOUBLE,
+        BeginDate DATETIME not null,
+        User_ID INTEGER not null,
+        primary key (ID)
     );
 
     create table Category (
         ID INTEGER NOT NULL AUTO_INCREMENT,
-       Name VARCHAR(20) not null,
-       Active TINYINT(1) default 1  not null,
-       User_ID INTEGER not null,
-       primary key (ID)
+        Name VARCHAR(20) not null,
+        Active TINYINT(1) default 1  not null,
+        User_ID INTEGER not null,
+        primary key (ID)
     );
 
     create table Detail (
         ID INTEGER NOT NULL AUTO_INCREMENT,
-       Description VARCHAR(50) not null,
-       Amount SMALLINT default 1  not null,
-       Value DOUBLE not null,
-       Move_ID INTEGER not null,
-       primary key (ID)
+        Description VARCHAR(50) not null,
+        Amount SMALLINT default 1  not null,
+        Value DOUBLE not null,
+        Move_ID INTEGER not null,
+        primary key (ID)
     );
 
     create table Month (
         ID INTEGER NOT NULL AUTO_INCREMENT,
-       Time SMALLINT not null,
-       Year_ID INTEGER not null,
-       primary key (ID)
+        Time SMALLINT not null,
+        Year_ID INTEGER not null,
+        primary key (ID)
     );
 
     create table Move (
         ID INTEGER NOT NULL AUTO_INCREMENT,
-       Description VARCHAR(50) not null,
-       Date DATETIME not null,
-       Nature INTEGER not null,
-       In_ID INTEGER,
-       Out_ID INTEGER,
-       Schedule_ID INTEGER,
-       Category_ID INTEGER not null,
-       primary key (ID)
+        Description VARCHAR(50) not null,
+        Date DATETIME not null,
+        Nature INTEGER not null,
+        In_ID INTEGER,
+        Out_ID INTEGER,
+        Schedule_ID INTEGER,
+        Category_ID INTEGER not null,
+        primary key (ID)
     );
 
     create table Schedule (
         ID INTEGER NOT NULL AUTO_INCREMENT,
-       Active TINYINT(1) default 1  not null,
-       Times SMALLINT not null,
-       Boundless TINYINT(1) not null,
-       Begin DATETIME not null,
-       Next DATETIME not null,
-       Frequency INTEGER not null,
-       User_ID INTEGER not null,
-       primary key (ID)
+        Active TINYINT(1) default 1  not null,
+        Times SMALLINT not null,
+        Boundless TINYINT(1) not null,
+        Begin DATETIME not null,
+        Next DATETIME not null,
+        Frequency INTEGER not null,
+        User_ID INTEGER not null,
+        primary key (ID)
     );
 
     create table Summary (
         ID INTEGER NOT NULL AUTO_INCREMENT,
-       In_ DOUBLE not null,
-       Out_ DOUBLE not null,
-       Nature INTEGER not null,
-       Month_ID INTEGER,
-       Year_ID INTEGER,
-       Category_ID INTEGER not null,
-       primary key (ID),
-      unique (Month_ID, Year_ID, Category_ID)
+        In_ DOUBLE not null,
+        Out_ DOUBLE not null,
+        Nature INTEGER not null,
+        Month_ID INTEGER,
+        Year_ID INTEGER,
+        Category_ID INTEGER not null,
+        primary key (ID),
+		unique (Month_ID, Year_ID, Category_ID)
     );
 
     create table User (
         ID INTEGER NOT NULL AUTO_INCREMENT,
-       Password VARCHAR(50) not null,
-       Email VARCHAR(50) not null unique,
-       Language VARCHAR(5) default 'pt-BR'  not null,
-       primary key (ID)
+        Password VARCHAR(50) not null,
+        Email VARCHAR(50) not null unique,
+        Language VARCHAR(5) default 'pt-BR'  not null,
+        primary key (ID)
     );
 
     create table Year (
         ID INTEGER NOT NULL AUTO_INCREMENT,
-       Time SMALLINT not null,
-       Account_ID INTEGER not null,
-       primary key (ID)
+        Time SMALLINT not null,
+        Account_ID INTEGER not null,
+        primary key (ID)
     );
 
     alter table Account 
