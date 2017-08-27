cc76bfcb57272c693ae3033091167ce38a376c55
Dara Keon <lc.sales.duarte@gmail.com>
2017-08-27 00:20:11 -0300

    Cookie encrypted
    by [Dara Keon (Ísis)] in [2013-09-27 20:34:51 (-03:00)] - hg hash {d5dda4877b25}

---
+++ site/DFM.Generic/AdminCookie.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Web;
+
+namespace Sulamerica.DomainModel.AutenticacaoAdmin
+{
+    public static class AdminCookie
+    {
+        internal static String Get()
+        {
+            var value = context == null ? local : get();
+
+            return Crypto.Decrypt(value);
+        }
+
+
+        private static String local;
+
+
+
+        private const String name = "CookieName";
+
+        private static HttpContext context
+        {
+            get { return HttpContext.Current; }
+        }
+
+        private static HttpCookieCollection requestCookies
+        {
+            get { return context.Request.Cookies; }
+        }
+
+        private static HttpCookieCollection responseCookies
+        {
+            get { return context.Response.Cookies; }
+        }
+
+
+        private static String get()
+        {
+            var cookie = requestCookies[name]
+                         ?? responseCookies[name];
+
+            if (cookie == null)
+                return null;
+
+            if (cookie.Value == null)
+                remove();
+
+            return cookie.Value;
+        }
+
+
+
+        public static void Set(String value)
+        {
+            remove();
+
+            value = Crypto.Encrypt(value);
+
+            if (context == null)
+            {
+                local = value;
+                return;
+            }
+
+            var cookie = new HttpCookie(name)
+                             {
+                                 Value = value,
+                                 Expires = DateTime.Now.AddDays(7)
+                             };
+
+            requestCookies.Add(cookie);
+            responseCookies.Add(cookie);
+
+            requestCookies[name].Value = value;
+            responseCookies[name].Value = value;
+        }
+
+
+        
+        public static void Clean()
+        {
+            remove();
+        }
+
+
+
+        private static void remove()
+        {
+            if (context == null)
+            {
+                local = null;
+                return;
+            }
+
+            if (requestCookies[name] != null)
+                requestCookies[name].Expires =
+                    DateTime.Now.AddMilliseconds(-1);
+
+            if (responseCookies[name] != null)
+                responseCookies[name].Expires =
+                    DateTime.Now.AddMilliseconds(-1);
+        }
+
+
+    }
+}
---
+++ site/DFM.Generic/TripleDESDisposable.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Sulamerica.DomainModel.AutenticacaoAdmin
+{
+    class TripleDESDisposable : IDisposable
+    {
+        private const String key = "";
+
+        private readonly TripleDESCryptoServiceProvider tripleDES;
+
+
+        internal TripleDESDisposable()
+        {
+            //if hashing was used get the hash code with regards to your key
+            var hashmd5 = new MD5CryptoServiceProvider();
+
+            var keyArray = hashmd5.ComputeHash(Encoding.UTF8.GetBytes(key));
+
+            //Always release the resources and flush data
+            // of the Cryptographic service provide. Best Practice
+            hashmd5.Clear();
+
+            tripleDES = new TripleDESCryptoServiceProvider
+            {
+                //set the secret key for the tripleDES algorithm
+                Key = keyArray,
+                //mode of operation. there are other 4 modes. 
+                //We choose ECB(Electronic code Book)
+                Mode = CipherMode.ECB,
+                Padding = PaddingMode.PKCS7
+            };
+        }
+        
+
+        internal byte[] Encrypt(byte[] toEncryptArray)
+        {
+            var cTransform = tripleDES.CreateEncryptor();
+
+            //transform the specified region of bytes array to resultArray
+            return cTransform.TransformFinalBlock(toEncryptArray, 0, toEncryptArray.Length);
+        }
+
+
+        internal byte[] Decrypt(byte[] toEncryptArray)
+        {
+            var cTransform = tripleDES.CreateDecryptor();
+
+            //transform the specified region of bytes array to resultArray
+            return cTransform.TransformFinalBlock(toEncryptArray, 0, toEncryptArray.Length);
+        }
+
+
+        internal void Dispose()
+        {
+            //Release resources held by TripleDes Encryptor                
+            tripleDES.Clear();
+        }
+
+
+    }
+}
