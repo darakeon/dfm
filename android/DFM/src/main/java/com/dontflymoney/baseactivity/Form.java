7318b60677d8ea3f1536155f0503ca9ad98b6c83
Dara Keon <lc.sales.duarte@gmail.com>
2017-08-28 00:38:15 -0300

    Removed unused methods
    by [Dara Keon (Isis Keon)] in [2017-02-09 23:25:15 (-02:00)] - hg hash {2d90b88f7590}

--- android/DFM/src/main/java/com/dontflymoney/baseactivity/Form.java
+++ android/DFM/src/main/java/com/dontflymoney/baseactivity/Form.java
@@ -5,7 +5,6 @@ import android.app.AlertDialog;
 import android.graphics.Color;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.dontflymoney.viewhelper.DialogSelectClickListener;
@@ -13,8 +12,6 @@ import com.dontflymoney.viewhelper.DialogSelectClickListener;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-
-import java.text.DecimalFormat;
 
 public class Form
 {
@@ -61,42 +58,6 @@ public class Form
 	private <T extends View> T getField(int id)
 	{
 		return (T)activity.findViewById(id);
-	}
-
-	public TextView createText(Double value, int gravity)
-	{
-		String text = new DecimalFormat("#,##0.00").format(value);
-
-		TextView field = createText(text, gravity);
-
-		int color = value < 0 ? Color.RED : Color.BLUE;
-		field.setTextColor(color);
-		
-		return field;
-	}
-
-	public TextView createText(String text, int gravity)
-	{
-		TextView field = new TextView(activity);
-
-		field.setText(text);
-		field.setGravity(gravity);
-		//TODO: put this on config
-		field.setTextSize(17);
-		field.setPadding(20, 20, 20, 20);
-		field.setTextColor(Color.BLACK);
-
-		return field;
-	}
-
-	public ImageView createImage(int resourceId)
-	{
-		ImageView field = new ImageView(activity);
-
-		field.setImageResource(resourceId);
-		field.setPadding(20, 20, 20, 20);
-
-		return field;
 	}
 
 
