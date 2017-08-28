be4cd9129346abadc49f6977dd53c8f37a87787e
Dara Keon <lc.sales.duarte@gmail.com>
2017-08-28 04:10:20 -0300

    Remove entirely retry of Volley
    by [Dara Keon (desktop)] in [2017-05-01 23:18:37 (-03:00)] - hg hash {813da0089f02}

--- android/DFM/src/main/kotlin/com/darakeon/dfm/api/InternalRequest.kt
+++ android/DFM/src/main/kotlin/com/darakeon/dfm/api/InternalRequest.kt
@@ -100,13 +100,7 @@ class InternalRequest<T : SmartStatic>(var activity: SmartActivity<T>, private v
 
 	private fun getDefaultRetryPolicy(step: Step): DefaultRetryPolicy {
 		val timeout = DefaultRetryPolicy.DEFAULT_TIMEOUT_MS
-
-		val maxRetries =
-			if (step == Step.Recording)
-				0
-			else
-				DefaultRetryPolicy.DEFAULT_MAX_RETRIES
-
+		val maxRetries = 0
 		val backoffMult = DefaultRetryPolicy.DEFAULT_BACKOFF_MULT
 
 		return DefaultRetryPolicy(timeout, maxRetries, backoffMult)
