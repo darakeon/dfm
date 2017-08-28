c61ad2f8d4a7747440766998de82c87491d5644c
Dara Keon <lc.sales.duarte@gmail.com>
2017-08-27 23:30:40 -0300

    SQL: Tinyint to bit /// Checked
    by [Dara Keon (Isis Keon)] in [2016-11-23 00:23:24 (-02:00)] - hg hash {cf81c1093c48}

---
+++ sql/2.1.0.0.sql
@@ -0,0 +1,32 @@
+
+
+alter table category
+	modify Active bit not null;
+
+alter table config
+	modify SendMoveEmail bit not null,
+    modify UseCategories bit not null;
+
+alter table schedule
+	modify Active bit not null,
+    modify ShowInstallment bit not null,
+    modify Boundless bit not null;
+
+alter table security
+	modify Active bit not null,
+    modify Sent bit not null;
+
+alter table summary
+	modify Broken bit not null;
+   
+alter table ticket
+	modify Active bit not null;
+
+alter table user
+	modify Active bit not null;
+
+
+
+alter table move
+	add Checked bit not null;
+
