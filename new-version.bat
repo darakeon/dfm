262d9a329eca110a66642631e9f83dc8809d11d2
Dara Keon <lc.sales.duarte@gmail.com>
2017-08-28 04:11:25 -0300

    Fix new version bat to do not execute with tag name
    by [Dara Keon (desktop)] in [2017-05-01 23:43:10 (-03:00)] - hg hash {b9482413e12c}

--- new-version.bat
+++ new-version.bat
@@ -1,6 +1,11 @@
-hg ci -m "New Version"
-hg tag %1
-hg up default
-hg merge develop
-hg ci -m "Publish"
-hg up develop
+if "%1" == "" (
+	echo "no tag"
+) else (
+	hg ci -m "New Version"
+	hg tag %1
+	hg up default
+	hg merge develop
+	hg ci -m "Publish"
+	hg up develop
+	hg push bitbucket
+)
