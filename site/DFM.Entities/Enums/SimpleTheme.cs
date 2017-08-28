978a5450737b68f374f8fc56c617b60b2b867c9b
Dara Keon <lc.sales.duarte@gmail.com>
2017-08-28 03:22:52 -0300

    Simple Theme (dark/light)
    by [Dara Keon (Isis Keon)] in [2017-03-26 04:08:15 (-03:00)] - hg hash {6ed3236d0eb4}

---
+++ site/DFM.Entities/Enums/SimpleTheme.cs
@@ -0,0 +1,20 @@
+﻿namespace DFM.Entities.Enums
+{
+	public enum SimpleTheme
+	{
+		Dark = 1,
+		None = 0,
+		Light = -1
+	}
+
+	public static class ThemeExtension
+	{
+		public static SimpleTheme Simplify(this BootstrapTheme theme)
+		{
+			return 
+				theme > 0 ? SimpleTheme.Dark : 
+				theme < 0 ? SimpleTheme.Light :
+							SimpleTheme.None;
+		}
+	}
+}
