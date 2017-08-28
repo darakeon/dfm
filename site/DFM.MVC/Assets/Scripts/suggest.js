11305f2da729bccad9b3bb96109131be956b0263
Dara Keon <lc.sales.duarte@gmail.com>
2017-08-28 02:17:52 -0300

    Create/Edit Account layout 'translated'
    by [Dara Keon (Isis Keon)] in [2017-03-12 14:41:19 (-03:00)] - hg hash {b81714d74c4f}

---
+++ site/DFM.MVC/Assets/Scripts/suggest.js
@@ -0,0 +1,20 @@
+﻿$(document).ready(function () {
+	$(".suggest-field").change(suggestUrl);
+});
+
+function suggestUrl(obj) {
+	var field = $(obj.target);
+	var name = field.val();
+
+	var targetField = $(field.data("target"));
+	var url = targetField.val();
+
+	if (name !== "" && url === "") {
+		url = name	
+			.toLowerCase()
+			.replace(/[ ]/g, "_")
+			.replace(/[^a-z0-9_]/g, "");
+
+		targetField.val(url);
+	}
+}
---
+++ site/DFM.MVC/Assets/Scripts/toggle.js
@@ -0,0 +1,13 @@
+﻿$(document).ready(function () {
+	$(".check-toggle").click(function () {
+		toggleFields($(this));
+	});
+
+	toggleFields($(".check-toggle"));
+});
+
+function toggleFields(checkBox) {
+	var hasLimit = checkBox.is(":checked");
+	var targetField = checkBox.data("target");
+	$(targetField).attr("disabled", !hasLimit);
+}
