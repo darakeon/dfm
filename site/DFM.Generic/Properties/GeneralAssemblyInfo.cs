713b37c527adbeff2870529b4c3ed2b1f40696cf
Dara Keon <lc.sales.duarte@gmail.com>
2017-08-28 01:29:27 -0300

    New version
    by [Dara Keon (desktop)] in [2017-02-15 01:56:53 (-02:00)] - hg hash {2160139e1fd8}

--- site/DFM.Generic/Properties/GeneralAssemblyInfo.cs
+++ site/DFM.Generic/Properties/GeneralAssemblyInfo.cs
@@ -16,5 +16,5 @@ using System.Runtime.InteropServices;
 //		Build Number
 //		Revision
 //
-[assembly: AssemblyVersion("2.1.4.3")]
-[assembly: AssemblyFileVersion("2.1.4.3")]
+[assembly: AssemblyVersion("2.1.4.4")]
+[assembly: AssemblyFileVersion("2.1.4.4")]
