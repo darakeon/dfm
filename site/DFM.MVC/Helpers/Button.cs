66a80c7695e9fc7e9ad54042214a1e90931748fe
Dara Keon <lc.sales.duarte@gmail.com>
2017-08-28 02:16:02 -0300

    Layout for Tables
    by [Dara Keon (Isis Keon)] in [2017-03-11 22:57:56 (-03:00)] - hg hash {12b9afc45237}

---
+++ site/DFM.MVC/Helpers/Button.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using JetBrains.Annotations;
+
+namespace DFM.MVC.Helpers
+{
+	public class Button
+	{
+		public String Text { get; private set; }
+		public String Action { get; private set; }
+		public Boolean PullLeft { get; private set; }
+
+		public Button(String text, [AspMvcAction] String action, Boolean pullLeft = false)
+		{
+			Text = text;
+			Action = action;
+			PullLeft = pullLeft;
+		}
+	}
+}
