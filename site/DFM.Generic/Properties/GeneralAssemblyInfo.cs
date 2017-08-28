25d7aa53aa1b0c0ff321143d48e494ecd6146dbd
Dara Keon <lc.sales.duarte@gmail.com>
2017-08-28 00:11:19 -0300

    Fixed site address
    by [Dara Keon (Isis Keon)] in [2017-01-17 00:26:01 (-02:00)] - hg hash {29e56015c2cd}

--- site/DFM.Generic/Properties/GeneralAssemblyInfo.cs
+++ site/DFM.Generic/Properties/GeneralAssemblyInfo.cs
@@ -16,5 +16,5 @@ using System.Runtime.InteropServices;
 //		Build Number
 //		Revision
 //
-[assembly: AssemblyVersion("2.1.3.0")]
-[assembly: AssemblyFileVersion("2.1.3.0")]
+[assembly: AssemblyVersion("2.1.3.1")]
+[assembly: AssemblyFileVersion("2.1.3.1")]
