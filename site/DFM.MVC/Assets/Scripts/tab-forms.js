a6473fc7ffcfbcb225c0e5dcff9f4c4fb4d87e6a
Dara Keon <lc.sales.duarte@gmail.com>
2017-08-28 02:13:29 -0300

    Refactor
    by [Dara Keon (Isis Keon)] in [2017-03-09 00:04:54 (-03:00)] - hg hash {d5d347b177f6}

---
+++ site/DFM.MVC/Assets/Scripts/tab-forms.js
@@ -0,0 +1,11 @@
+﻿$(document).ready(function () {
+	$(".nav-tabs li").click(function () {
+		var href = $(this).find("a").attr("href");
+		var action = $(href).data("form");
+		$("form").attr("action", action);
+		$("#ActiveForm").val(href.substr(1));
+	});
+
+	var tabCaller = "." + $("#ActiveForm").val().toLowerCase() + "-caller";
+	$(tabCaller).find("a").click();
+});
