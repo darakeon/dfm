c800d9b0ea5750546575b5f0847fef756a88180b
Dara Keon <lc.sales.duarte@gmail.com>
2017-08-28 02:33:02 -0300

    Year Report layout 'translated'
    by [Dara Keon (Isis Keon)] in [2017-03-14 03:01:22 (-03:00)] - hg hash {9dbc9f587c9b}

---
+++ site/DFM.MVC/Helpers/Views/YearUnit.cs
@@ -0,0 +1,46 @@
+using System;
+
+namespace DFM.MVC.Helpers.Views
+{
+	public class YearUnit
+	{
+		public YearUnit(Int32 year)
+		{
+			Year = year;
+		}
+
+		public YearUnit(DateTime date)
+			: this(date.Year) { }
+
+		public Int32 Year { get; }
+
+		public static YearUnit operator +(YearUnit unit, Int32 years)
+		{
+			var date = unit.ToDate().AddYears(years);
+			return new YearUnit(date);
+		}
+
+		public static Boolean operator <(YearUnit unit, YearUnit other)
+		{
+			return unit.ToDate() < other.ToDate();
+		}
+
+		public static Boolean operator >(YearUnit unit, YearUnit other)
+		{
+			return unit.ToDate() > other.ToDate();
+		}
+
+		public String Label => $"{Year}";
+		public String Url => $"{Year}";
+
+		public override string ToString()
+		{
+			return Label;
+		}
+
+		public DateTime ToDate()
+		{
+			return new DateTime(Year, 1, 1);
+		}
+	}
+}
