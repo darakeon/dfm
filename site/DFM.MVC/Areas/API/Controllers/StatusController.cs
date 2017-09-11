891b05e853dae6d2f0402cd40d27f602e3186d50
lucas_carol <lc.sales.duarte@gmail.com>
2017-09-10 22:09:08 -0300

    security: add site checking to app
    It check and wakes up the site, if the IIS stopped site because no one
    was using

---
+++ site/DFM.MVC/Areas/API/Controllers/StatusController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace DFM.MVC.Areas.API.Controllers
+{
+    public class StatusController : BaseJsonController
+	{
+        public ActionResult Index()
+        {
+            return JsonGet(() => new { status = "online" });
+        }
+    }
+}
