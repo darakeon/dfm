5bbc04d58190931607bda2d9eb5c16aa81bcda66
lucas_carol <lc.sales.duarte@gmail.com>
2017-10-07 22:29:11 -0300

    accessibility: update colors with icons
    Put icons where there was just colors to tell the meaning

---
+++ site/DFM.MVC/Models/ValueWithSign.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DFM.MVC.Models
+{
+	public class ValueWithSign
+	{
+		public ValueWithSign(Decimal value, String language, Boolean differentIfZero = false)
+		{
+			Value = value;
+			Language = language;
+			DifferentIfZero = differentIfZero;
+		}
+
+		public Decimal Value { get; }
+		public String Language { get; }
+		public Boolean DifferentIfZero { get; }
+	}
+}
