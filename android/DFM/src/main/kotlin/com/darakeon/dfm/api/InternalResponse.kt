038adb4b7cf5e8a3a210c32cca44775102a71f5a
lucas_carol <lc.sales.duarte@gmail.com>
2017-10-22 23:20:37 -0200

    chore: refactor request

--- android/DFM/src/main/kotlin/com/darakeon/dfm/api/InternalResponse.kt
+++ android/DFM/src/main/kotlin/com/darakeon/dfm/api/InternalResponse.kt
@@ -3,21 +3,20 @@ package com.darakeon.dfm.api
 import org.json.JSONObject
 
 internal class InternalResponse {
-	private var result: JSONObject? = null
-	private var error: String? = null
+	private val result: JSONObject?
+	private val error: String?
 
 	constructor(result: JSONObject) {
 		this.result = result
+		this.error = null
 	}
 
 	constructor(error: String) {
+		this.result = null
 		this.error = error
 	}
-
 
 	fun isSuccess(): Boolean = error == null
 	fun getResult(): JSONObject = result!!
 	fun getError(): String = error!!
-
-
 }
