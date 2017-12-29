79857a9235d02fb188838b4d779d6f1afa80bb76
lucas_carol <lc.sales.duarte@gmail.com>
2017-12-29 19:31:50 -0200

    chore: refactor config tests

--- site/DFM.Generic/Cfg.cs
+++ site/DFM.Generic/Cfg.cs
@@ -8,6 +8,11 @@ namespace DFM.Generic
 	{
 		private static NameValueCollection appSettings => ConfigurationManager.AppSettings;
 
+		public static String Server = appSettings["Server"];
+		public static String DataBase = appSettings["DataBase"];
+		public static String Login = appSettings["Login"];
+		public static String Password = appSettings["Password"];
+
 		public static String EmailSender
 		{
 			get { return appSettings["EmailSender"]; }
@@ -21,10 +26,5 @@ namespace DFM.Generic
 		public static Boolean IsLocal => appSettings["IsLocal"] == "1";
 
 		public static Int32 PasswordErrorLimit => Int32.Parse(appSettings["PasswordErrorLimit"]);
-
-		public static String Get(String key)
-		{
-			return appSettings[key];
-		}
 	}
 }
