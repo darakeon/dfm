7e8cb627a17aee49be29029d228dd1eb87c520c4
lucas_carol <lc.sales.duarte@gmail.com>
2018-01-05 04:47:23 -0200

    chore: remove console test project

--- site/DFM.Console4Tests/Properties/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("DFM.Console4Tests")]
-[assembly: AssemblyProduct("DFM.Console4Tests")]
-[assembly: Guid("cabdca21-a02b-4b96-a040-05aeeaabc6bd")]
