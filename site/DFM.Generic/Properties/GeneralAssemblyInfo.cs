b8d9dda61be6a6554fee448acecc990e313c112f
lucas_carol <lc.sales.duarte@gmail.com>
2018-03-02 23:44:24 -0300

    version: update to 4.1.2.0

--- site/DFM.Generic/Properties/GeneralAssemblyInfo.cs
+++ site/DFM.Generic/Properties/GeneralAssemblyInfo.cs
@@ -16,5 +16,5 @@ using System.Runtime.InteropServices;
 //		Build Number
 //		Revision
 //
-[assembly: AssemblyVersion("4.1.1.3")]
-[assembly: AssemblyFileVersion("4.1.1.3")]
+[assembly: AssemblyVersion("4.1.2.0")]
+[assembly: AssemblyFileVersion("4.1.2.0")]
