fe62b0107baf36202745c3999fc2fc87aa5ccff8
lucas_carol <lc.sales.duarte@gmail.com>
2018-05-26 22:59:18 -0300

    android-api: add ux handling to wait and error

--- android/DFM/src/main/kotlin/com/darakeon/dfm/api/Handler.kt
+++ android/DFM/src/main/kotlin/com/darakeon/dfm/api/Handler.kt
@@ -1,38 +1,84 @@
 package com.darakeon.dfm.api
 
+import android.app.Activity
+import android.content.DialogInterface
+import com.darakeon.dfm.R
+import com.darakeon.dfm.dialogs.alertError
+import com.darakeon.dfm.extensions.composeErrorEmail
+import com.darakeon.dfm.extensions.logoutLocal
+import com.darakeon.dfm.extensions.redirect
+import com.darakeon.dfm.tfa.TFAActivity
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
 internal class Handler<T>(
-	private val onSuccess: (T) -> Unit,
-	private val onError: (Throwable) -> Unit
+	private val context: Activity,
+	private val onSuccess: (T) -> Unit
 )  : Callback<Body<T>> {
 	override fun onResponse(call: Call<Body<T>>?, response: Response<Body<T>>?) {
+		val url = getUrl(call)
+
 		if (response == null) {
-			onError(Exception("Null response"))
+			onError(url, Exception("Null response"))
 			return
 		}
 
 		val body = response.body()
 
 		if (body == null) {
-			onError(Exception("Null body"))
+			onError(url, Exception("Null body"))
 			return
 		}
 
 		if (body.data == null) {
-			onError(Exception("[${body.code}] ${body.error}"))
+			assemblyResponse(body.code, body.error)
 			return
 		}
 
 		onSuccess(body.data.accountList)
+
+		context.endUIWait()
 	}
 
-	override fun onFailure(call: Call<Body<T>>?, throwable: Throwable?) =
+	override fun onFailure(call: Call<Body<T>>?, throwable: Throwable?) {
+		val url = getUrl(call)
+
 		if (throwable == null) {
-			onError(Exception("Null error"))
+			onError(url, Exception("Null error"))
 		} else {
-			onError(throwable)
+			onError(url, throwable)
 		}
+	}
+
+	private fun getUrl(call: Call<Body<T>>?): String {
+		return call?.request()?.url()?.encodedPath() ?: "No url info"
+	}
+
+	private fun onError(url: String?, error: Throwable) {
+		val sendReport = DialogInterface.OnClickListener(function = { dialog, _ ->
+			dialog.dismiss()
+			context.composeErrorEmail(url ?: "", error)
+		})
+
+		context.alertError(context.getString(R.string.error_contact_url), sendReport)
+	}
+
+	private fun assemblyResponse(
+		code: Int?,
+		error: String?
+	) {
+		val tfa = context.resources.getInteger(R.integer.TFA)
+		val uninvited = context.resources.getInteger(R.integer.uninvited)
+
+		if (code == tfa) {
+			context.redirect<TFAActivity>()
+		} else {
+			context.alertError(error ?: "")
+
+			if (code == uninvited) {
+				context.logoutLocal()
+			}
+		}
+	}
 }
