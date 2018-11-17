1130df6e699ab4cee5e85a5ac10c1bf4c01d77a8
Dara Keon <lc.sales.duarte@gmail.com>
2018-11-17 04:36:04 -0200

    chore: remove unnecessary class

--- android/DFM/src/main/kotlin/com.darakeon.dfm/extensions/EditText.kt
+++ android/DFM/src/main/kotlin/com.darakeon.dfm/extensions/EditText.kt
@@ -1,14 +1,18 @@
 package com.darakeon.dfm.extensions
 
+import android.text.Editable
+import android.text.TextWatcher
 import android.widget.EditText
-import com.darakeon.dfm.moves.AfterTextWatcher
 
 fun EditText.onChange(
 	onTextChanged: (String) -> Unit
 ) {
-	addTextChangedListener(object : AfterTextWatcher() {
-		override fun textChanged(text: String) {
-			onTextChanged(text)
+	addTextChangedListener(object : TextWatcher {
+		override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) { }
+		override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) { }
+
+		override fun afterTextChanged(field: Editable?) {
+			onTextChanged(field.toString())
 		}
 	})
 }
