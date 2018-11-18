950206c351af94ade33d8a45b6aaafa0e2777dd2
Dara Keon <lc.sales.duarte@gmail.com>
2018-11-18 17:46:08 -0200

    android-tfa: remove static class

--- android/DFM/src/main/kotlin/com.darakeon.dfm/tfa/TFAActivity.kt
+++ android/DFM/src/main/kotlin/com.darakeon.dfm/tfa/TFAActivity.kt
@@ -3,13 +3,14 @@ package com.darakeon.dfm.tfa
 import android.view.View
 import com.darakeon.dfm.R
 import com.darakeon.dfm.accounts.AccountsActivity
+import com.darakeon.dfm.api.old.DELETE
 import com.darakeon.dfm.api.old.InternalRequest
 import com.darakeon.dfm.auth.auth
 import com.darakeon.dfm.base.BaseActivity
 import com.darakeon.dfm.extensions.redirect
 import kotlinx.android.synthetic.main.tfa.code
 
-class TFAActivity : BaseActivity<TFAStatic>(TFAStatic) {
+class TFAActivity : BaseActivity<DELETE>(DELETE) {
 	override val contentView = R.layout.tfa
 	override val hasTitle: Boolean get() = false
 
--- android/DFM/src/main/kotlin/com.darakeon.dfm/tfa/TFAStatic.kt
+++
@@ -1,8 +0,0 @@
-package com.darakeon.dfm.tfa
-
-import com.darakeon.dfm.base.SmartStatic
-
-object TFAStatic : SmartStatic
-{
-	override var succeeded: Boolean = false
-}
