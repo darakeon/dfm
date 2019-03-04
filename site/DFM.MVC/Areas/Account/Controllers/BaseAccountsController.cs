2f171aa83a130d1434ecf285af949c0bf994d209
Dara Keon <lc.sales.duarte@gmail.com>
2019-03-04 18:33:05 -0300

    accounts: fix request for non-existent account
    Redirect to home and show warning

--- site/DFM.MVC/Areas/Account/Controllers/BaseAccountsController.cs
+++ site/DFM.MVC/Areas/Account/Controllers/BaseAccountsController.cs
@@ -1,11 +1,37 @@
 ﻿using System.Web.Mvc;
+using DFM.BusinessLogic.Exceptions;
 using DFM.MVC.Areas.Account.Models;
 using DFM.MVC.Helpers.Controllers;
+using DFM.MVC.Helpers.Global;
 
 namespace DFM.MVC.Areas.Account.Controllers
 {
 	public class BaseAccountsController : BaseController
 	{
+		protected override void OnException(ExceptionContext filterContext)
+		{
+			if (
+				filterContext.Exception is DFMCoreException exception
+				&&
+				exception.Type == ExceptionPossibilities.InvalidAccount
+			)
+			{
+				ErrorAlert.Add(exception.Type);
+
+				filterContext.Result = 
+					RedirectToRoute(
+						RouteNames.DEFAULT, 
+						new { controller = "Accounts" }
+					);
+
+				filterContext.ExceptionHandled = true;
+			}
+			else
+			{
+				base.OnException(filterContext);
+			}
+		}
+
 		protected ActionResult createEditSchedule(BaseMovesModel model)
 		{
 			if (ModelState.IsValid)
