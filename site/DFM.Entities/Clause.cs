b9cdd5bdcfdfdd26cf78cc1d144eb821a56b498d
Dara Keon <lc.sales.duarte@gmail.com>
2019-09-11 16:48:47 -0300

    safe: move contract clauses to DB
    If it is needed to prove something, it must be at DB
    
    And now, to add new contract, it's only needed to add to DB

---
+++ site/DFM.Entities/Clause.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DFM.Entities
+{
+	public class Clause
+	{
+		public Clause()
+		{
+			Items = new List<Clause>();
+		}
+
+		public String Text { get; set; }
+		public IList<Clause> Items { get; set; }
+	}
+}
---
+++ site/DFM.Entities/Terms.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Keon.Util.DB;
+using Newtonsoft.Json;
+
+namespace DFM.Entities
+{
+	public class Terms : IEntityLong
+	{
+		public virtual Int64 ID { get; set; }
+		public virtual String Language { get; set; }
+		public virtual String Json { get; set; }
+
+		public virtual Clause GetClause()
+		{
+			return JsonConvert
+				.DeserializeObject<Clause>(Json);
+		}
+	}
+}
