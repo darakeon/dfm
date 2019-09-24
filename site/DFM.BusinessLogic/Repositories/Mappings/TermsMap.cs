e9740b73f0a61bff0d78198c8d7cdebf83fa8d68
Dara Keon <lc.sales.duarte@gmail.com>
2019-09-24 18:14:27 -0300

    contract: add mapping for terms
    Avoid any change to entity

---
+++ site/DFM.BusinessLogic/Repositories/Mappings/TermsMap.cs
@@ -0,0 +1,16 @@
+﻿using DFM.Entities;
+using FluentNHibernate.Automapping;
+using FluentNHibernate.Automapping.Alterations;
+
+namespace DFM.BusinessLogic.Repositories.Mappings
+{
+	// ReSharper disable once UnusedMember.Global
+	public class TermsMap : IAutoMappingOverride<Terms>
+	{
+		public void Override(AutoMapping<Terms> mapping)
+		{
+			mapping.Map(m => m.Json).Not.Update();
+			mapping.Map(m => m.Language).Not.Update();
+		}
+	}
+}
