958dd3caa9ea7586effce8ed0323e886e9705ac8
Dara Keon <lc.sales.duarte@gmail.com>
2019-10-02 00:32:13 -0300

    browser-tests: add user logon

--- site/Tests/Browser/users.test.js
+++ site/Tests/Browser/users.test.js
@@ -7,6 +7,10 @@ describe('Users', () => {
 		await db.cleanup()
 	}, 30000)
 
+	beforeEach(async () => {
+		await db.cleanupTickets()
+	})
+
 	test('SignUp', async () => {
 		await page.goto('http://localhost:2709/Users/SignUp')
 		await page.waitForSelector('#body form')
@@ -22,5 +26,39 @@ describe('Users', () => {
 
 		const message = await page.$eval(alertBox, n => n.innerHTML)
 		await expect(message).toContain('Cadastro efetuado com sucesso.')
+	})
+
+	test('Logon', async () => {
+		const email = 'logon_success@dontflymoney.com'
+		await db.createUserIfNotExists(email, 1)
+
+		await page.goto('http://localhost:2709/Users/Logon')
+		await page.waitForSelector('#body form')
+
+		await page.type('#Email', email)
+		await page.type('#Password', db.password.plain)
+		await page.click('#RememberMe')
+		await page.click('#body form button[type="submit"]')
+
+		await expect(page.title()).resolves.toMatch('DfM - Contas')
+	})
+
+	test('Logon not active', async () => {
+		const email = 'logon_not_active@dontflymoney.com'
+		await db.createUserIfNotExists(email)
+
+		await page.goto('http://localhost:2709/Users/Logon')
+		await page.waitForSelector('#body form')
+
+		await page.type('#Email', email)
+		await page.type('#Password', db.password.plain)
+		await page.click('#RememberMe')
+		await page.click('#body form button[type="submit"]')
+
+		const alertBox = '.alert'
+		await page.waitForSelector(alertBox)
+
+		const message = await page.$eval(alertBox, n => n.innerHTML)
+		await expect(message).toContain('Seu usuário não foi ativado.')
 	})
 })
