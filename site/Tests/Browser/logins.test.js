49ea832d4c19ea9b0806e8e39ec4eb139e7fabe9
Dara Keon <lc.sales.duarte@gmail.com>
2019-11-16 10:46:16 -0300

    browser-tests: add logins

---
+++ site/Tests/Browser/logins.test.js
@@ -0,0 +1,28 @@
+const puppeteer = require('puppeteer')
+const fs = require('fs')
+
+const db = require('./db.js')
+const puppy = require('./puppy.js')
+
+describe('Logins', () => {
+	let user = {};
+
+	beforeAll(async () => {
+		await db.cleanup()
+		user = await puppy.logon('logins@dontflymoney.com')
+	}, 30000)
+
+	test('Delete', async () => {
+		await puppy.call('Logins')
+
+		const cookies = await page.cookies()
+		const cookie = cookies.find(c => c.name == "DFM")
+		const id = cookie.value.substring(0, 5)
+
+		await puppy.submit(`/Logins/Delete/${id}`)
+
+		expect(page.url()).toEqual(
+			'http://localhost:2709/Users/LogOn?ReturnUrl=%2fLogins'
+		)
+	})
+})
