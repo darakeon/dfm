e2bd11d2677a6db91e9a499311f17632e602ccee
Dara Keon <lc.sales.duarte@gmail.com>
2019-11-15 23:52:25 -0300

    browser-tests: add ops

---
+++ site/Tests/Browser/ops.test.js
@@ -0,0 +1,53 @@
+const puppeteer = require('puppeteer')
+const fs = require('fs')
+
+const db = require('./db.js')
+const puppy = require('./puppy.js')
+
+describe('Ops', () => {
+	beforeAll(async () => {
+		await db.cleanup()
+	}, 30000)
+
+	test('Index', async () => {
+		await puppy.call('Ops')
+
+		const warning = await puppy.content('.alert')
+		expect(warning).toContain('Ocorreu um erro interno. Por favor, entre em contato conosco.')
+	})
+
+	test('Code 404', async () => {
+		await puppy.call('Ops/Code/404')
+
+		const warning = await puppy.content('.alert')
+		expect(warning).toContain('Página não encontrada.')
+	})
+
+	test('Code 500', async () => {
+		await puppy.call('Ops/Code/500')
+
+		const warning = await puppy.content('.alert')
+		expect(warning).toContain('Ocorreu um erro interno. Por favor, entre em contato conosco.')
+	})
+
+	test('Code Unknown', async () => {
+		await puppy.call('Ops/Code/100')
+
+		const warning = await puppy.content('.alert')
+		expect(warning).toContain('Ocorreu um erro interno. Por favor, entre em contato conosco.')
+	})
+
+	test('Error Uninvited', async () => {
+		await puppy.call('Ops/Error/207')
+
+		const warning = await puppy.content('.alert')
+		expect(warning).toContain('Tchau!')
+	})
+
+	test('Do not hack', async () => {
+		await puppy.call('Ops/Help')
+
+		const warning = await puppy.content('.panel-body')
+		expect(warning).toContain('https://github.com/darakeon/dfm')
+	})
+})
