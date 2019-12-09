e156439b29703067b535f74f60ab1fbe8b00ccf3
Dara Keon <lc.sales.duarte@gmail.com>
2019-12-08 22:23:14 -0300

    browser-test: add deploy tests project
    To use msbuild to deploy, had to remove mvcbuildviews, but browser
    tests should already tests all the views.
    
    Shortcut is there to run the bat as administrator automatically

--- site/Tests/Browser/test-all.bat
+++ site/Tests/Browser/test-all.bat
@@ -1,5 +1,13 @@
 @echo off
 
+cd %~dp0
+
+cd ..\..\MVC
+
+msbuild MVC.csproj /p:DeployOnBuild=True /p:PublishProfile="Browser Tests" /p:Configuration=BrowserTests -verbosity:quiet
+
+cd ..\Tests\Browser
+
 REM Run sequentially because of page sessions
 REM There are tests that login and logoff
 npm test -- --runInBand
