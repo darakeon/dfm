3d5f1565f5d99cc71278dbe0217ad6b0acc8d03a
Dara Keon <lc.sales.duarte@gmail.com>
2020-01-28 02:00:02 -0300

    version: add numbers to .net core

--- version/src/csharp.rs
+++ version/src/csharp.rs
@@ -2,28 +2,42 @@ use std::fs;
 
 use crate::version::Version;
 
-static PATH: &str =
-	r"..\site\Generic\Properties\GeneralAssemblyInfo.cs";
-
 pub fn update_csharp(version: &Version) {
+	update_csharp_file(&version, r"..\site\AssetPackages\AssetPackages.csproj");
+	update_csharp_file(&version, r"..\site\Authentication\Authentication.csproj");
+	update_csharp_file(&version, r"..\site\BusinessLogic\BusinessLogic.csproj");
+	update_csharp_file(&version, r"..\site\Email\Email.csproj");
+	update_csharp_file(&version, r"..\site\Entities\Entities.csproj");
+	update_csharp_file(&version, r"..\site\Generic\Generic.csproj");
+	update_csharp_file(&version, r"..\site\Language\Language.csproj");
+	update_csharp_file(&version, r"..\site\MVC\MVC.csproj");
+	update_csharp_file(&version, r"..\site\Tests\BusinessLogic\BusinessLogic.Tests.csproj");
+	update_csharp_file(&version, r"..\site\Tests\Email\Email.Tests.csproj");
+	update_csharp_file(&version, r"..\site\Tests\Language\Language.Tests.csproj");
+	update_csharp_file(&version, r"..\site\Tests\MVC\MVC.Tests.csproj");
+	update_csharp_file(&version, r"..\site\Tests\Util\Tests.Util.csproj");
+}
+
+fn update_csharp_file(version: &Version, file: &str) {
 	let old_assembly = assembly_version(&version.prev);
 	let new_assembly = assembly_version(&version.code);
 
 	let old_file = assembly_file_version(&version.prev);
 	let new_file = assembly_file_version(&version.code);
-
-	let content = fs::read_to_string(PATH)
+	
+	let content = fs::read_to_string(file)
 		.unwrap()
 		.replace(&old_assembly, &new_assembly)
 		.replace(&old_file, &new_file);
 
-	fs::write(PATH, content).expect("error on c# recording");
+	fs::write(file, content)
+		.expect(&format!("error on c# {} recording", file));
 }
 
 fn assembly_version(version: &str) -> String {
-	format!(r#"[assembly: AssemblyVersion("{}")]"#, version)
+	format!(r#"    <AssemblyVersion>{}</AssemblyVersion>"#, version)
 }
 
 fn assembly_file_version(version: &str) -> String {
-	format!(r#"[assembly: AssemblyFileVersion("{}")]"#, version)
+	format!(r#"<FileVersion>{}</FileVersion>"#, version)
 }
