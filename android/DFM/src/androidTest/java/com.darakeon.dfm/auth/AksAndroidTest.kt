83804fc16518e18ba9898f639744a38c02919e72
Dara Keon <lc.sales.duarte@gmail.com>
2020-02-19 00:47:57 -0300

    android: refactor encrypt / decrypt tests
    Separated in two classes because versions from api 23 to above doesn't
    need context.
    
    I was trying to make this a normal automated test instead of needing
    device connected, but KeyStore class throw an error out of device.

--- android/DFM/src/androidTest/java/com.darakeon.dfm/auth/AksAndroidTest.kt
+++ android/DFM/src/androidTest/java/com.darakeon.dfm/auth/AksAndroidTest.kt
@@ -1,20 +1,27 @@
 package com.darakeon.dfm.auth
 
 import android.support.test.InstrumentationRegistry
-import android.support.test.runner.AndroidJUnit4
 import org.junit.Assert
 import org.junit.Test
-import org.junit.runner.RunWith
 
-@RunWith(AndroidJUnit4::class)
-class AksTest {
+class AksAndroidTest {
+	@Test
+	fun encryptionAndDecryption_23AndSoOn() {
+		val aks = Aks()
+
+		val message = "Hey, listen!"
+
+		val cipher = aks.encrypt(message)
+		val result = aks.decrypt(cipher)
+
+		Assert.assertNotEquals(message, cipher)
+		Assert.assertEquals(message, result)
+	}
 
 	@Test
-	fun encryptionAndDecryption() {
+	fun encryptionAndDecryption_below23() {
 		val context = InstrumentationRegistry.getTargetContext()
-		Assert.assertNotNull(context)
-
-		val aks = Aks(context)
+		val aks = OldAks(context)
 
 		val message = "Hey, listen!"
 
