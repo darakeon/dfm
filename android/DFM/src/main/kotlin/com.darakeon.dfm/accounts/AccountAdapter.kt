b54ab12874b20f9f36c85ae7056d4f6f5bcc1cf9
Dara Keon <lc.sales.duarte@gmail.com>
2020-02-20 23:27:20 -0300

    android-ui: refactor adapters

--- android/DFM/src/main/kotlin/com.darakeon.dfm/accounts/AccountAdapter.kt
+++ android/DFM/src/main/kotlin/com.darakeon.dfm/accounts/AccountAdapter.kt
@@ -1,34 +1,16 @@
 package com.darakeon.dfm.accounts
 
-import android.content.Context
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.widget.BaseAdapter
 import com.darakeon.dfm.R
 import com.darakeon.dfm.api.entities.accounts.Account
-import com.darakeon.dfm.auth.getThemeLineColor
+import com.darakeon.dfm.base.Adapter
 
 class AccountAdapter(
-	context: Context,
+	activity: AccountsActivity,
 	private val accountList: Array<Account>
-) : BaseAdapter() {
-	private var inflater: LayoutInflater =
-		context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+) : Adapter<AccountsActivity, Account, AccountLine>(activity, accountList) {
+	override val id: Int
+		get() = R.layout.accounts_line
 
-	override fun getCount(): Int = accountList.size
-	override fun getItem(position: Int): Any = position
-	override fun getItemId(position: Int): Long = position.toLong()
-
-	override fun getView(position: Int, view: View?, viewGroup: ViewGroup): View? {
-		val line = createOrGet(view) as AccountLine
-
-		val color = getThemeLineColor(position)
-		line.setAccount(accountList[position], color)
-
-		return line
-	}
-
-	private fun createOrGet(view: View?) =
-		(view ?: inflater.inflate(R.layout.accounts_line, null))
+	override fun populateView(view: AccountLine, position: Int) =
+		view.setAccount(accountList[position])
 }
--- android/DFM/src/main/kotlin/com.darakeon.dfm/extract/MoveAdapter.kt
+++ android/DFM/src/main/kotlin/com.darakeon.dfm/extract/MoveAdapter.kt
@@ -1,30 +1,20 @@
 package com.darakeon.dfm.extract
 
-import android.content.Context
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.widget.BaseAdapter
 import com.darakeon.dfm.R
 import com.darakeon.dfm.api.entities.extract.Move
-import com.darakeon.dfm.auth.getThemeLineColor
+import com.darakeon.dfm.base.Adapter
 
-class MoveAdapter(private val activity: ExtractActivity, moveJsonList: Array<Move>, private val canCheck: Boolean) : BaseAdapter() {
+class MoveAdapter(
+	private val activity: ExtractActivity,
+	moveJsonList: Array<Move>,
+	private val canCheck: Boolean
+) : Adapter<ExtractActivity, Move, MoveLine>(activity, moveJsonList) {
+	private val moveList: MutableList<Move> =
+		moveJsonList.toMutableList()
 
-	private val moveList: MutableList<Move> = moveJsonList.toMutableList()
-	private var inflater: LayoutInflater = activity.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+	override val id: Int
+		get() = R.layout.extract_line
 
-	override fun getCount(): Int = moveList.size
-	override fun getItem(position: Int): Any = position
-	override fun getItemId(position: Int): Long = position.toLong()
-
-	override fun getView(position: Int, view: View?, viewGroup: ViewGroup): View? {
-		val line = inflater.inflate(R.layout.extract_line, null) as MoveLine
-
-		val color = getThemeLineColor(position)
-		line.setMove(activity, moveList[position], color, canCheck)
-
-		return line
-	}
-
+	override fun populateView(view: MoveLine, position: Int) =
+		view.setMove(activity, moveList[position], canCheck)
 }
