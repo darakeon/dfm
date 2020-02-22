7bf7aff1b431140e46c66ef7d31e66c9eee4da35
Dara Keon <lc.sales.duarte@gmail.com>
2020-02-22 01:43:18 -0300

    android: refactor details comparison

--- android/DFM/src/main/kotlin/com.darakeon.dfm/api/entities/moves/Detail.kt
+++ android/DFM/src/main/kotlin/com.darakeon.dfm/api/entities/moves/Detail.kt
@@ -1,14 +1,21 @@
 package com.darakeon.dfm.api.entities.moves
 
-class Detail {
-	var description: String? = null
-	var amount: Int = 0
-	var value: Double = 0.0
-
-	internal fun equals(description: String?, amount: Int, value: Double): Boolean {
-		return this.description == description
-				&& this.amount == amount
-				&& this.value == value
+class Detail(
+	val description: String? = null,
+	val amount: Int = 0,
+	val value: Double = 0.0
+) {
+	override fun equals(other: Any?): Boolean {
+		return other is Detail
+			&& description == other.description
+			&& amount == other.amount
+			&& value == other.value
 	}
 
+	override fun hashCode(): Int {
+		var result = description?.hashCode() ?: 0
+		result = 31 * result + amount
+		result = 31 * result + value.hashCode()
+		return result
+	}
 }
