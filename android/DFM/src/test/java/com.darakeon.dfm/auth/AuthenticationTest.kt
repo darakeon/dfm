2f4bdb91e6226218726c130550eed237d29f7261
Dara Keon <lc.sales.duarte@gmail.com>
2020-02-29 01:33:13 -0300

    android-tests: add authentication

---
+++ android/DFM/src/test/java/com.darakeon.dfm/auth/AuthenticationTest.kt
@@ -0,0 +1,30 @@
+package com.darakeon.dfm.auth
+
+import com.darakeon.dfm.utils.MockContext
+import org.hamcrest.CoreMatchers.`is`
+import org.hamcrest.MatcherAssert.assertThat
+import org.junit.Before
+import org.junit.Test
+
+class AuthenticationTest {
+	private lateinit var auth : Authentication
+
+	@Before
+	fun setup() {
+		val mockContext = MockContext().mockSharedPreferences()
+		auth = Authentication(mockContext.activity)
+	}
+
+	@Test
+	fun setAndGetTicket() {
+		auth.ticket = "new-ticket"
+		assertThat(auth.ticket, `is`("new-ticket"))
+	}
+
+	@Test
+	fun clearTicket() {
+		auth.ticket = "new-ticket"
+		auth.clear()
+		assertThat(auth.ticket, `is`(""))
+	}
+}
