9c043e4761fc20861a3f2b86adb456032b19fc17
Dara Keon <lc.sales.duarte@gmail.com>
2020-03-25 04:24:19 -0300

    android-tests: add login activity

---
+++ android/DFM/src/test/java/com/darakeon/dfm/login/LoginActivityTest.kt
@@ -0,0 +1,56 @@
+package com.darakeon.dfm.login
+
+import android.view.View
+import com.darakeon.dfm.R
+import com.darakeon.dfm.auth.Authentication
+import com.darakeon.dfm.extensions.getPrivate
+import com.darakeon.dfm.utils.activity.ActivityMock
+import com.darakeon.dfm.utils.activity.getActivityName
+import com.darakeon.dfm.utils.robolectric.simulateNetwork
+import org.hamcrest.CoreMatchers.`is`
+import org.hamcrest.MatcherAssert.assertThat
+import org.junit.Assert.assertNotNull
+import org.junit.Assert.assertTrue
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.Shadows.shadowOf
+
+@RunWith(RobolectricTestRunner::class)
+class LoginActivityTest {
+	private lateinit var mocker: ActivityMock
+
+	@Before
+	fun setup() {
+		mocker = ActivityMock()
+	}
+
+	@Test
+	fun structure() {
+		val activity = mocker.create<LoginActivity>()
+		assertNotNull(activity.findViewById(R.id.email))
+		assertNotNull(activity.findViewById(R.id.password))
+		assertNotNull(activity.findViewById(R.id.login_button))
+	}
+
+	@Test
+	fun login() {
+		mocker.server.enqueue("login")
+
+		val activity = mocker.create<LoginActivity>()
+		activity.simulateNetwork()
+
+		val view = View(activity)
+
+		activity.login(view)
+
+		val auth = activity.getPrivate("auth") as Authentication
+		assertTrue(auth.isLoggedIn)
+
+		val shadow = shadowOf(activity)
+		val intent = shadow.peekNextStartedActivity()
+
+		assertThat(intent.getActivityName(), `is`("WelcomeActivity"))
+	}
+}
