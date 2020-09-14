77d633f0c7e716b84afea4f3b3a22fff3b8f2b8d
Dara Keon <lc.sales.duarte@gmail.com>
2020-09-14 01:27:52 -0300

    chore: fix lint issues

--- android/ErrorLogs/src/main/kotlin/com/darakeon/dfm/error_logs/StartActivity.kt
+++ android/ErrorLogs/src/main/kotlin/com/darakeon/dfm/error_logs/StartActivity.kt
@@ -1,11 +1,11 @@
 package com.darakeon.dfm.error_logs
 
+import android.app.Activity
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.darakeon.dfm.error_logs.service.SiteErrorService
 
-class StartActivity : AppCompatActivity() {
+class StartActivity : Activity() {
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.activity_start)
