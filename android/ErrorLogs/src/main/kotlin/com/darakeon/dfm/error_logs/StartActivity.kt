11849ebaba003ad80dffac96aa7f1481aea1e401
Dara Keon <lc.sales.duarte@gmail.com>
2020-09-14 06:30:04 -0300

    android: add login to error logs app

--- android/ErrorLogs/src/main/kotlin/com/darakeon/dfm/error_logs/StartActivity.kt
+++ android/ErrorLogs/src/main/kotlin/com/darakeon/dfm/error_logs/StartActivity.kt
@@ -1,16 +1,48 @@
 package com.darakeon.dfm.error_logs
 
-import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import com.darakeon.dfm.error_logs.service.SiteErrorService
+import com.darakeon.dfm.lib.api.entities.login.Login
+import kotlinx.android.synthetic.main.activity_start.email
+import kotlinx.android.synthetic.main.activity_start.password
+import kotlinx.android.synthetic.main.activity_start.tfa
 
-class StartActivity : Activity() {
+class StartActivity : BaseActivity() {
+
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.activity_start)
 
-		val intent = Intent(this, SiteErrorService::class.java)
-		startService(intent)
+		if (auth.isLoggedIn) {
+			startService()
+		}
+	}
+
+	private fun startService() {
+		startService(Intent(this, SiteErrorService::class.java))
+		finish()
+	}
+
+	@Suppress("UNUSED_PARAMETER")
+	fun login(view: View) {
+		if (auth.isLoggedIn) {
+			proceedLogin(Login(auth.ticket))
+		} else {
+			api.login(
+				email.text.toString(),
+				password.text.toString(),
+				this::proceedLogin
+			)
+		}
+	}
+
+	private fun proceedLogin(it: Login) {
+		auth.ticket = it.ticket
+		api.validateTFA(
+			tfa.text.toString(),
+			this::startService
+		)
 	}
 }
