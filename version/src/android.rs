eee80477dbb8c50678426b6f9334259ce259ac9d
Dara Keon <lc.sales.duarte@gmail.com>
2020-09-15 02:04:19 -0300

    version: fix changer to android new arch

--- version/src/android.rs
+++ version/src/android.rs
@@ -4,34 +4,53 @@ use std::fs;
 
 use crate::version::Version;
 
-static PATH: &str = r"..\android\DFM\build.gradle";
+static PATH_MAIN: &str = r"..\android\build.gradle";
+static PATH_APP: &str = r"..\android\App\build.gradle";
+static PATH_ERROR_LOGS: &str = r"..\android\ErrorLogs\build.gradle";
 
 pub fn update_android(version: &Version) {
-	let mut content = fs::read_to_string(PATH).unwrap();
+	let changed = update_main(version);
+	
+	if !changed {
+		return
+	}
+	
+	let args: Vec<String> = env::args().collect();
+	let change_android = args.len() < 2 || args[1] != "--no-android";
+
+	if !change_android {
+		return
+	}
+	
+	change_code(PATH_APP, "(2011\\d{6})");
+	change_code(PATH_ERROR_LOGS, "(\\d+)");
+}
+
+fn update_main(version: &Version) -> bool {
+	let mut content = fs::read_to_string(PATH_MAIN).unwrap();
 
 	if !content.contains(&version.prev) {
-		return;
+		return false;
 	}
-
+	
 	let old_name = version_name(&version.prev);
 	let new_name = version_name(&version.code);
 
 	content = content.replace(&old_name, &new_name);
 
-	content = change_code(content);
-
-	fs::write(PATH, content).expect("error on android recording");
+	fs::write(PATH_MAIN, content).expect("error on android recording");
+	
+	return true
 }
 
-fn change_code(content: String) -> String {
-	let args: Vec<String> = env::args().collect();
-	let change_android = args.len() < 2 || args[1] != "--no-android";
+fn version_name(version: &str) -> String {
+	format!(r#"ext.dfm_version = "{}""#, version)
+}
 
-	if !change_android {
-		return content;
-	}
+fn change_code(path: &str, pattern: &str) {
+	let mut content = fs::read_to_string(path).unwrap();
 
-	let pattern = version_code("(2011\\d{6})");
+	let pattern = version_code(&pattern);
 	let regex = Regex::new(&pattern).unwrap();
 
 	let old_number = regex
@@ -48,11 +67,9 @@ fn change_code(content: String) -> String {
 	let old_code = version_code(&old_number.to_string());
 	let new_code = version_code(&new_number.to_string());
 
-	content.replace(&old_code, &new_code)
-}
-
-fn version_name(version: &str) -> String {
-	format!(r#"versionName "{}""#, version)
+	content = content.replace(&old_code, &new_code);
+	
+	fs::write(path, content).expect("error on android recording");
 }
 
 fn version_code(version: &str) -> String {
