62c6aefd0ca4fe88d76da3d8de83ea21d0cc6c58
Dara Keon <lc.sales.duarte@gmail.com>
2020-09-24 20:39:03 -0300

    tests: update ci to android 29
    CircleCI was downloading api 29 into api 28 machine

--- docker/android.bat
+++ docker/android.bat
@@ -1,2 +1,2 @@
 docker rm andy
-docker run --name andy -i -t -v C:\Lucas\Git\Me\Dfm:/dfm -w /dfm/android circleci/android:api-28
+docker run --name andy -i -t -v C:\Lucas\Git\Me\Dfm:/dfm -w /dfm/android circleci/android:api-29
