760bcaedafc921e3c5dbd66d00fd824773ecba87
Dara Keon <lc.sales.duarte@gmail.com>
2020-10-05 17:53:35 -0300

    site: reallocate requests from users
    Users was not the right controller for them, they are generic request
    from site.

---
+++ site/MVC/Controllers/GenericController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text;
+using DFM.Generic;
+using DFM.MVC.Helpers.Controllers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DFM.MVC.Controllers
+{
+	public class GenericController : Controller
+	{
+		[HttpGetAndHead]
+		public IActionResult Mobile()
+		{
+			return Redirect(Cfg.GooglePlay);
+		}
+
+		public IActionResult Reload()
+		{
+			return Content(" ");
+		}
+
+	}
+}
