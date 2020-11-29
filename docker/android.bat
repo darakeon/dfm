53f9ce26b09d928b2261557bc1b667d4a19ce12c
Dara Keon <lc.sales.duarte@gmail.com>
2020-11-28 21:35:36 -0300

    android: fix script of running docker
    Need to delete files so build inside docker can create them

--- docker/android.bat
+++ docker/android.bat
@@ -1,2 +1,4 @@
 docker rm andy
+del ..\android\Lib\src\debug\res\xml\network_security_config.xml
+del ..\android\Lib\src\debug\res\values\site-address.xml
 docker run --name andy -it -v %~dp0..\:/dfm -w /dfm/android circleci/android:api-29
