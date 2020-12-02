14ece0928a78d97d3baf6e472ede9eb70a5b9a95
Dara Keon <lc.sales.duarte@gmail.com>
2020-12-02 19:28:23 -0300

    ci: name net browser tests machine
    browser > bowser > yoshi is my favorite of mario

--- docker/net-run.bat
+++ docker/net-run.bat
@@ -1,3 +1,2 @@
-@echo off
-docker container prune %1
-docker run -it -v %~dp0..\:/var/dfm -w /var/dfm/site/Tests/Browser --expose 2709 -P darakeon/browser-tests
+docker rm yoshi
+docker run --name yoshi -it -v %~dp0..\:/var/dfm -w /var/dfm/site/Tests/Browser --expose 2709 -P darakeon/browser-tests
