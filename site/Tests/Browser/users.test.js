27bb913e2e4080a8aec6f2642ad71e7f17c26518
Dara Keon <lc.sales.duarte@gmail.com>
2020-12-03 03:31:29 -0300

    tests: separate browser tests 'token' from 'user'

--- site/Tests/Browser/users.test.js
+++ site/Tests/Browser/users.test.js
@@ -1,5 +1,4 @@
 const puppeteer = require('puppeteer')
-const fs = require('fs')
 const db = require('./db')
 const puppy = require('./puppy')
 
@@ -65,32 +64,6 @@ describe('Users', () => {
 		await expect(message).toContain('Seu acesso ao sistema não foi ativado ainda.')
 	})
 
-	test('Token User Activate - by url', async () => {
-		const email = 'token_activate_user@dontflymoney.com'
-		await db.createUserIfNotExists(email)
-		const token = await db.createToken(email, 0)
-
-		await puppy.call(`Tokens/UserVerification/${token}`)
-
-		const message = await puppy.content('.alert')
-		await expect(message).toContain('Verifição feita com sucesso.')
-	})
-
-	test('Token User Activate - by form', async () => {
-		const email = 'token_activate_user@dontflymoney.com'
-		await db.createUserIfNotExists(email)
-		const token = await db.createToken(email, 0)
-
-		await puppy.call('Tokens')
-		await page.waitForSelector('#body form')
-
-		await page.type('#Token', token)
-		await page.click('#body form button[type="submit"]')
-
-		const message = await puppy.content('.alert')
-		await expect(message).toContain('Verifição feita com sucesso.')
-	})
-
 	test('Forgot Password', async () => {
 		const email = 'forgot_password@dontflymoney.com'
 		await db.createUserIfNotExists(email)
@@ -115,54 +88,6 @@ describe('Users', () => {
 
 		const message = await puppy.content('.alert')
 		await expect(message).toContain('Se existir este e-mail no sistema, você receberá as instruções para prosseguir.')
-	})
-
-	test('Token Password Reset - by url', async () => {
-		const email = 'token_reset_password@dontflymoney.com'
-		await db.createUserIfNotExists(email, 1)
-		const token = await db.createToken(email, 1)
-
-		await puppy.call(`Tokens/PasswordReset/${token}`)
-		await page.waitForSelector('#body form')
-
-		await page.type('#Password', db.password.plain)
-		await page.type('#RetypePassword', db.password.plain)
-		await page.click('#body form button[type="submit"]')
-
-		const message = await puppy.content('.alert')
-		await expect(message).toContain('Senha redefinida com sucesso.')
-	})
-
-	test('Token Password Reset - by form', async () => {
-		const email = 'token_reset_password@dontflymoney.com'
-		await db.createUserIfNotExists(email, 1)
-		const token = await db.createToken(email, 1)
-
-		await puppy.call('Tokens')
-		await page.waitForSelector('#body form')
-
-		await page.type('#Token', token)
-		await page.select('#SecurityAction', "PasswordReset")
-		await page.click('#body form button[type="submit"]')
-		await page.waitForSelector('#body form')
-
-		await page.type('#Password', db.password.plain)
-		await page.type('#RetypePassword', db.password.plain)
-		await page.click('#body form button[type="submit"]')
-
-		const message = await puppy.content('.alert')
-		await expect(message).toContain('Senha redefinida com sucesso.')
-	})
-
-	test('Token Disable', async () => {
-		const email = 'token_disable@dontflymoney.com'
-		await db.createUserIfNotExists(email, 1)
-		const token = await db.createToken(email, 1)
-
-		await puppy.call(`Tokens/Disable/${token}`)
-
-		const message = await puppy.content('.alert')
-		await expect(message).toContain('Token desativado com sucesso.')
 	})
 
 	test('LogOff', async () => {
