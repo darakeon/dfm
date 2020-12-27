4d6e39d97a3291e8054c6d291a63f3125aad9656
Dara Keon <lc.sales.duarte@gmail.com>
2020-12-27 00:13:18 -0300

    site: add to month report sum of values when click

---
+++ site/MVC/Assets/scripts/table-sum.js
@@ -0,0 +1,30 @@
+﻿$(document).ready(function () {
+	let total = 0;
+
+	$(".sum-factor").click(function () {
+		const factor = $(this).hasClass("active") ? -1 : 1
+
+		$(this).toggleClass("active")
+
+		const parent = $(this).data("parent")
+		const anyActive = $(parent + " .active").length > 0
+
+		if (!anyActive) {
+			total = 0
+			$(".top-right-highlight").html("")
+			return
+		}
+
+		const value =
+			$(this).find(".value")
+				.html()
+				.replace(",", ".")
+
+		total += parseFloat(value) * factor
+		total = Math.round(total * 100) / 100
+
+		$(".top-right-highlight").html(
+			total.toLocaleString(lang, { minimumFractionDigits: 2 })
+		)
+	})
+})
