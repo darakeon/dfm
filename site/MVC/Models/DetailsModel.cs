71bcdedef307754fa005fc112ca5ad4e3c98a3dd
Dara Keon <lc.sales.duarte@gmail.com>
2020-12-31 00:27:11 -0300

    site: add modal for move details

---
+++ site/MVC/Models/DetailsModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using DFM.BusinessLogic.Response;
+
+namespace DFM.MVC.Models
+{
+	public class DetailsModel
+	{
+		public DetailsModel(MoveInfo move, String language)
+		{
+			Guid = move.Guid;
+			Description = move.Description;
+			Value = move.Value;
+			Language = language;
+			DetailList = move.DetailList;
+		}
+
+		public Guid Guid { get; }
+		public String Description { get; }
+		public Decimal Value { get; }
+		public String Language { get; }
+		public IList<DetailInfo> DetailList { get; }
+	}
+}
