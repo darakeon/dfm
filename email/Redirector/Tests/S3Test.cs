2f2d15ae0170e9ec66b8549a1f998194ed2f11af
Dara Keon <lc.sales.duarte@gmail.com>
2021-01-21 21:42:38 -0300

    mail: fix status check

---
+++ email/Redirector/Tests/S3Test.cs
@@ -0,0 +1,12 @@
+﻿namespace Redirector.Tests
+{
+	class S3Test
+	{
+		public static void Test()
+		{
+			var s3 = new S3();
+			s3.GetFile("AMAZON_SES_SETUP_NOTIFICATION");
+			s3.DeleteFile("AMAZON_SES_SETUP_NOTIFICATION");
+		}
+	}
+}
