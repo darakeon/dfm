28ee02bebc879d1c90067500b02df0749e36101d
Dara Keon <lc.sales.duarte@gmail.com>
2021-03-07 02:57:58 -0300

    site: fix moves sum value
    Clicking at specific buttons should not activate/deactivate that move at
    sum popup

--- site/MVC/Assets/scripts/table-sum.js
+++ site/MVC/Assets/scripts/table-sum.js
@@ -1,7 +1,17 @@
 ﻿$(document).ready(function () {
 	let total = 0
 
-	$('.sum-factor').click(function () {
+	$('.sum-factor').click(function (e) {
+		let target = $(e.target)
+		const id = $(this).attr('id')
+
+		while (target.attr('id') !== id) {
+			if ($(target).attr('role') === 'button')
+				return;
+
+			target = target.parent()
+		}
+
 		const factor = $(this).hasClass('active') ? -1 : 1
 
 		$(this).toggleClass('active')
