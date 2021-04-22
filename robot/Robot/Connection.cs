86682c23da069899d068c48cb8d38a35ea152537
Dara Keon <lc.sales.duarte@gmail.com>
2021-04-21 23:50:51 -0300

    robot: create program connected to DB

---
+++ robot/Robot/Connection.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using DFM.BusinessLogic.Repositories.Mappings;
+using DFM.Entities;
+using DFM.Generic;
+using Keon.NHibernate.Schema;
+using Keon.NHibernate.Sessions;
+
+namespace DFM.Robot
+{
+	class Connection
+	{
+		private const String envVarName = "ASPNETCORE_ENVIRONMENT";
+		private const String sessionName = "ROBOT";
+
+		private static readonly String name = 
+			Environment.GetEnvironmentVariable(envVarName);
+
+		public static void Run(Action action)
+		{
+			Cfg.Init(name);
+
+			sessionFactory(() => session(action));
+		}
+
+		private static void sessionFactory(Action action)
+		{
+			SessionFactoryManager.Initialize<UserMap, User>(Cfg.DB);
+
+			try
+			{
+				action();
+			}
+			finally
+			{
+				SessionFactoryManager.End();
+			}
+		}
+
+		private static void session(Action action)
+		{
+			SessionManager.Init(() => sessionName);
+
+			try
+			{
+				action();
+			}
+			finally
+			{
+				SessionManager.Close();
+			}
+		}
+
+	}
+}
