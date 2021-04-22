90265a83dc1a66e9b50fdcee12288e2b74ef128d
Dara Keon <lc.sales.duarte@gmail.com>
2021-04-21 23:50:51 -0300

    robot: create program connected to DB

---
+++ robot/Robot/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using DFM.Authentication;
+using DFM.BusinessLogic;
+using DFM.Entities.Enums;
+
+namespace DFM.Robot
+{
+	class Program
+	{
+		public static void Main()
+		{
+			Connection.Run(() =>
+			{
+				var services = new ServiceAccess(getTicket, getSite);
+
+				services.Robot.RunSchedule();
+
+				Console.WriteLine("Hello World!");
+			});
+		}
+
+		private static ClientTicket getTicket(Boolean remember)
+		{
+			return new ClientTicket("ROBOT", TicketType.Local);
+		}
+
+		private static string getSite()
+		{
+			return "https://dontflymoney.com";
+		}
+	}
+}
