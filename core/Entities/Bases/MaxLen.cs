8ccc67abcef01bdeb015ef38b43135a28296dcd6
Dara Keon <lc.sales.duarte@gmail.com>
2021-05-20 23:19:29 -0300

    safe: fix e-mail size limit
    The maximum size of an e-mail is 320 characters

--- core/Entities/Bases/MaxLen.cs
+++ core/Entities/Bases/MaxLen.cs
@@ -13,7 +13,7 @@ namespace DFM.Entities.Bases
 		public const Int16 SecurityToken = 50;
 		public const Int16 TicketKey = 52;
 		public const Int16 UserPassword = 60;
-		public const Int16 UserEmail = 50;
+		public const Int16 UserEmail = 320;
 		public const Int16 ConfigLanguage = 5;
 
 	}
