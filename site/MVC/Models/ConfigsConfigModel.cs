795ddc9acacc9803eea71808bd08630f7e171c1b
Dara Keon <lc.sales.duarte@gmail.com>
2021-06-24 18:04:03 -0300

    site: split main config into its own page

--- site/MVC/Models/ConfigsConfigModel.cs
+++ site/MVC/Models/ConfigsConfigModel.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using DFM.MVC.Models.Configs;
 
 namespace DFM.MVC.Models
@@ -7,31 +8,22 @@ namespace DFM.MVC.Models
 	{
 		public ConfigsConfigModel()
 		{
-			Main = new MainConfig(admin, current, translator, errorAlert);
 			Info = new UserInfo(safe, translator, errorAlert);
 			TFA = new TFAForm(safe, current, translator, errorAlert);
 			ThemeOpt = new ThemeOptions(admin, Theme, translator, errorAlert);
 		}
 
-		public MainConfig Main { get; set; }
 		public UserInfo Info { get; set; }
 		public TFAForm TFA { get; set; }
 		public ThemeOptions ThemeOpt { get; set; }
 
 		public Form ActiveForm { get; set; }
 
-		public String LanguageFieldName =>
-			getName<ConfigsConfigModel>(m => m.Main.Language);
-
 		public String BackTo { get; set; }
-
-		public String BackFieldName =>
-			getName<ConfigsConfigModel>(m => m.BackTo);
 
 		// ReSharper disable once UnusedMember.Global
 		public enum Form
 		{
-			Main,
 			Password,
 			Email,
 			Theme,
---
+++ site/MVC/Models/ConfigsModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DFM.MVC.Models
+{
+	public interface ConfigsModel
+	{
+		String BackTo { get; }
+		IList<String> Save();
+	}
+}
