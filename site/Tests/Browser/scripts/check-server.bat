349339f25e6266f94be2ed5cf17aba1f51b68e36
Dara Keon <lc.sales.duarte@gmail.com>
2021-06-28 16:48:42 -0300

    tests: keep cmd if site of browser tests fail

--- site/Tests/Browser/scripts/check-server.bat
+++ site/Tests/Browser/scripts/check-server.bat
@@ -5,10 +5,11 @@ echo %1
 curl -s http://localhost:2709 > NUL
 if errorlevel 1 (
 	if "%1" == ".........." (
-		exit 1
+		echo Cannot find server
+		pause
 	) else (
 		call %~dp0\check-server %1.
 	)
 ) else (
-	echo "Woke up!"
+	echo Woke up!
 )
