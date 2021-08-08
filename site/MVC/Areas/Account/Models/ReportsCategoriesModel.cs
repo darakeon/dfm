590624fa52597e551cbe06f761d6a87f318f90ef
Dara Keon <lc.sales.duarte@gmail.com>
2021-08-08 18:03:43 -0300

    site: add screen report by category

---
+++ site/MVC/Areas/Account/Models/ReportsCategoriesModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using DFM.BusinessLogic.Response;
+using DFM.MVC.Helpers.Models;
+
+namespace DFM.MVC.Areas.Account.Models
+{
+	public class ReportsCategoriesModel : BaseAccountModel
+	{
+		public ReportsCategoriesModel(Int32? id)
+		{
+			var dateMonth = DateFromInt.GetDateMonth(id, now);
+			var dateYear = DateFromInt.GetDateYear(id, now);
+
+			var data = report.GetCategoryReport(CurrentAccountUrl, dateYear, dateMonth);
+
+			ChartData = data.List;
+
+			Month = dateMonth;
+			Year = dateYear;
+		}
+
+		public IList<CategoryValue> ChartData { get; }
+
+		public IList<CategoryValue> ChartDataOut =>
+			ChartData.Where(d => d.Out > 0).ToList();
+
+		public IList<CategoryValue> ChartDataIn =>
+			ChartData.Where(d => d.In > 0).ToList();
+
+		public Int32 Month { get; }
+		public Int32 Year { get; }
+
+		public String Date =>
+			String.Format(
+				translator["ShortDateFormat"],
+				translator.GetMonthName(Month),
+				Year
+			);
+	}
+}
--- site/Tests/Browser/reports.test.js
+++ site/Tests/Browser/reports.test.js
@@ -3,19 +3,17 @@ const puppy = require('./puppy.js')
 
 describe('Reports', () => {
 	let user = {};
-	let accountIn = {};
-	let accountOut = {};
+	let account = {};
 	let category = {};
 
 	beforeAll(async () => {
 		user = await puppy.logon('reports@dontflymoney.com')
-		accountIn = await db.createAccountIfNotExists('Account In', user)
-		accountOut = await db.createAccountIfNotExists('Account Out', user)
-		category = await db.createCategoryIfNotExists('Category Move', user)
+		account = await db.createAccountIfNotExists('Account Reports', user)
+		category = await db.createCategoryIfNotExists('Category Reports', user)
 	})
 
 	test('Index', async () => {
-		const url = `Account/${accountOut}/Reports`
+		const url = `Account/${account}/Reports`
 
 		await puppy.call(url)
 		const alert = await puppy.content('#body .alert')
@@ -25,7 +23,7 @@ describe('Reports', () => {
 	})
 
 	test('Month', async () => {
-		const url = `Account/${accountOut}/Reports/Month/201911`
+		const url = `Account/${account}/Reports/Month/201911`
 
 		await puppy.call(url)
 		const alert = await puppy.content('#body .alert')
@@ -36,7 +34,7 @@ describe('Reports', () => {
 		const id = await puppy.createMove(
 			db,
 			'Move for Month', '2019-11-16', '1,00',
-			category, accountOut, null,
+			category, account, null,
 			user
 		)
 
@@ -44,15 +42,15 @@ describe('Reports', () => {
 		const table = await puppy.content('#body .table')
 		expect(table).toContain(`<td>Move for Month</td>`)
 
-		const editUrl = `Account/${accountOut}/Moves/Edit/${id}`
+		const editUrl = `Account/${account}/Moves/Edit/${id}`
 		expect(table).toContain(editUrl)
 
-		const deleteUrl = `Account/${accountOut}/Moves/Delete/${id}`
+		const deleteUrl = `Account/${account}/Moves/Delete/${id}`
 		expect(table).toContain(deleteUrl)
 	})
 
 	test('Year', async () => {
-		const url = `Account/${accountOut}/Reports/Year/2018`
+		const url = `Account/${account}/Reports/Year/2018`
 
 		await puppy.call(url)
 		const alert = await puppy.content('#body .alert')
@@ -63,7 +61,7 @@ describe('Reports', () => {
 		await puppy.createMove(
 			db,
 			'Move for Year', '2018-11-16', '1,00',
-			category, accountOut, null
+			category, account, null
 		)
 
 		await puppy.call(url)
@@ -85,7 +83,7 @@ describe('Reports', () => {
 		const id = await puppy.createMove(
 			db,
 			'Search target', '2020-10-01', '24,00',
-			category, accountOut, null
+			category, account, null
 		)
 
 		await puppy.call()
@@ -102,7 +100,43 @@ describe('Reports', () => {
 		const line = await puppy.content(`#body .table #m${id}`)
 		expect(line).toContain('Search target')
 
-		const reportUrl = `Account/${accountOut}/Reports/Month/202010`
+		const reportUrl = `Account/${account}/Reports/Month/202010`
 		expect(line).toContain(reportUrl)
+	})
+
+	test('Categories', async () => {
+		const url = `Account/${account}/Reports/Categories/202108`
+
+		await puppy.call(url)
+		const alert = await puppy.content('#body .alert')
+		expect(alert).toContain(
+			`Não há Movimentações para este mês.`
+		)
+
+		const valueOut = '19,86'
+		await puppy.createMove(
+			db,
+			'Move for Categories Out', '2021-08-08', valueOut,
+			category, account, null,
+			user
+		)
+
+		await puppy.call(url)
+		let chartOut = await puppy.content('#body #container-out')
+		chartOut = chartOut.replace('<tspan dy="14" x="5">​</tspan>', ' ')
+		expect(chartOut).toContain(`${category}: ${valueOut}`)
+
+		const valueIn = '27,03'
+		await puppy.createMove(
+			db,
+			'Move for Categories In', '2021-08-08', valueIn,
+			category, null, account,
+			user
+		)
+
+		await puppy.call(url)
+		let chartIn = await puppy.content('#body #container-in')
+		chartIn = chartIn.replace('<tspan dy="14" x="5">​</tspan>', ' ')
+		expect(chartIn).toContain(`${category}: ${valueIn}`)
 	})
 })
