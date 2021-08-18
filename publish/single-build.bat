412439866f3c026172f32da9427c3db2f3f34683
Dara Keon <lc.sales.duarte@gmail.com>
2021-08-18 14:36:34 -0300

    publish: force no cache to get updates

--- publish/single-build.bat
+++ publish/single-build.bat
@@ -4,7 +4,7 @@ set dir=%~dp0
 set machine=%~1
 set push=%~2
 
-docker build .. -t darakeon/dfm-%machine% -f "%dir%dfm-%machine%.dockerfile"
+docker build .. --pull -t darakeon/dfm-%machine% -f "%dir%dfm-%machine%.dockerfile"
 
 if "%push%" == "push" (
 	docker push darakeon/dfm-%machine%
