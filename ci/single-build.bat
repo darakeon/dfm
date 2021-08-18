2acacd393b203f734df44b33ca670b62dc05c2b0
Dara Keon <lc.sales.duarte@gmail.com>
2021-08-18 14:36:08 -0300

    ci: force no cache to get updates

--- ci/single-build.bat
+++ ci/single-build.bat
@@ -4,7 +4,7 @@ set machine=%~1
 set push=%~2
 
 docker context use default
-docker build . -f %machine%.dockerfile -t darakeon/%machine% --network=host
+docker build . --pull -f %machine%.dockerfile -t darakeon/%machine% --network=host
 docker image prune -f
 
 if "%push%" == "push" (
