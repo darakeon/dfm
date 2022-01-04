6da676904b1dfc63f34feed8a8bb3eb39fb426bb
Dara Keon <lc.sales.duarte@gmail.com>
2022-01-04 10:31:46 -0300

    site: make wizard highlight just show at text end

--- site/MVC/Assets/scripts/wizard.js
+++ site/MVC/Assets/scripts/wizard.js
@@ -11,41 +11,25 @@ function nextWizard() {
 	if (current.length === 0 || current.html() === current.data('text')) {
 
 		if (wizardCount + 1 < wizardMax) {
-			wizardCount++
-
-			current.html('')
-
-			var element = $('#wizard-text-' + wizardCount)
-			var text = element.data('text')
-
-			addText(element, text, 1)
+			changeMessage(current)
 		}
 
-		$('.wizard-highlight-' + (wizardCount - 1)).each(
-			function (_, obj) {
-				$(obj).css('borderColor', $(obj).data('borderColor'))
-				$(obj).css('borderWidth', $(obj).data('borderWidth'))
-			}
-		)
+		clearHighlight()
 
-		$('.wizard-highlight-' + wizardCount).each(
-			function (_, obj) {
-				$(obj).data('borderColor', $(obj).css('borderColor'))
-				$(obj).css('borderColor', 'var(--highlight-0)')
-
-				var borderWidth = '7px'
-
-				$(obj).animate({
-					borderWidth: borderWidth,
-				}, 500, 'swing', function() {
-					$(obj).data('borderWidth', $(obj).css('borderWidth'))
-					$(obj).css('borderWidth', borderWidth)
-				})
-			}
-		)
 	} else {
 		printAll()
 	}
+}
+
+function changeMessage(current) {
+	wizardCount++
+
+	current.html('')
+
+	var element = $('#wizard-text-' + wizardCount)
+	var text = element.data('text')
+
+	addText(element, text, 1)
 }
 
 function addText(element, text, chars) {
@@ -54,8 +38,37 @@ function addText(element, text, chars) {
 	setTimeout(() => {
 		if (element.html() !== text) {
 			addText(element, text, ++chars)
+		} else {
+			addHighlight()
 		}
 	}, 50)
+}
+
+function clearHighlight() {
+	$('.wizard-highlight-' + (wizardCount - 1)).each(
+		function (_, obj) {
+			$(obj).css('borderColor', $(obj).data('borderColor'))
+			$(obj).css('borderWidth', $(obj).data('borderWidth'))
+		}
+	)
+}
+
+function addHighlight() {
+	$('.wizard-highlight-' + wizardCount).each(
+		function (_, obj) {
+			$(obj).data('borderColor', $(obj).css('borderColor'))
+			$(obj).css('borderColor', 'var(--highlight-0)')
+
+			var borderWidth = '7px'
+
+			$(obj).animate({
+				borderWidth: borderWidth,
+			}, 500, 'swing', function () {
+				$(obj).data('borderWidth', $(obj).css('borderWidth'))
+				$(obj).css('borderWidth', borderWidth)
+			})
+		}
+	)
 }
 
 $(document).ready(function () {
