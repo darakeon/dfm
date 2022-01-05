9e02c7b3d127ff016ea5358e63ad233025e6c8b3
Dara Keon <lc.sales.duarte@gmail.com>
2022-01-05 11:33:00 -0300

    publish: change windows scripts to linux
    Docker Desktop is not free anymore to windows

--- publish/build-all.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-set dir=%~dp0
-set push=%~1
-
-call single-build.bat site %push%
-call single-build.bat robot %push%
---
+++ publish/build-all.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+./single-build.sh site $1
+./single-build.sh robot $1
