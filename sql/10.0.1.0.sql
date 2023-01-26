f9c32a348d1b00e54019c4e13d2ed169e7e002e7
Dara Keon <lc.sales.duarte@gmail.com>
2023-01-26 20:41:29 -0300

    db: fix trigger names

--- sql/10.0.1.0.sql
+++ sql/10.0.1.0.sql
@@ -4,3 +4,18 @@ insert into migrations (name) values ('10.0.1.0');
 
 create view deleted_users_wipe as
 	select id, email, when_ as `when`, why, s3, password, tfa from wipe;
+
+
+drop trigger no_update_purge;
+create trigger no_update_wipe
+	before update
+	on `wipe` for each row
+		signal sqlstate '45000'
+			set message_text = 'cannot update wipe';
+
+drop trigger no_delete_purge;
+create trigger no_delete_wipe
+	before delete
+	on `wipe` for each row
+		signal sqlstate '45000'
+			set message_text = 'cannot delete wipe';
