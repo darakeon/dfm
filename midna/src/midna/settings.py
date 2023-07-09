a02ff7fb61413725f85d49873a6bf8d8c6ddcd68
Dara Keon <lc.sales.duarte@gmail.com>
2023-07-09 23:12:06 +0100

    midna: create folder for static files
    They need to be exported from docker machine so nginx serve them
    separately

--- midna/src/midna/settings.py
+++ midna/src/midna/settings.py
@@ -137,6 +137,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = "/var/midna/static/inside"
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
