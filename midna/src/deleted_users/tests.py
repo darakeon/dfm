b557b74b866a201214463d32b456af611ad19e0a
Dara Keon <lc.sales.duarte@gmail.com>
2023-07-24 16:40:17 +0100

    midna: remove email from wipe

--- midna/src/deleted_users/tests.py
+++ midna/src/deleted_users/tests.py
@@ -4,18 +4,6 @@ from deleted_users.models import Wipe
 from utils.crypt import check
 
 class WipeTests(TestCase):
-	def test_encrypt_email(self):
-		not_hashed_email = 'email@domain.com'
-
-		wipe = Wipe(email=not_hashed_email)
-
-		assert wipe.hashed_email == ''
-
-		wipe.encrypt_email()
-
-		assert check(not_hashed_email, wipe.hashed_email)
-		assert wipe.email == None
-
 	def test_hashed_email_base64(self):
 		original = "$2b$12$ibiSaL7EiAFxPcPwDmx6vOZrnpEvUjEh95vzKCV.jj5445YlVCmJK"
 		expected = "JDJiJDEyJGliaVNhTDdFaUFGeFBjUHdEbXg2dk9acm5wRXZVakVoOTV2ektDVi5qajU0NDVZbFZDbUpL"
