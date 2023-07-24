20301773fab9b1d83b877ce99bef69c5ac7bd6f0
Dara Keon <lc.sales.duarte@gmail.com>
2023-07-24 14:45:47 +0100

    midna: remove fix
    Already executed, not needed anymore

--- midna/src/deleted_users/views.py
+++ midna/src/deleted_users/views.py
@@ -5,31 +5,3 @@ from deleted_users.models import Wipe
 from utils.s3 import S3
 
 
-@login_required
-def fix(_):
-	wipes = Wipe.objects.filter(s3__contains='@')
-
-	response = []
-
-	s3 = S3()
-
-	for wipe in wipes:
-		email = wipe.email
-
-		email = wipe.s3.split('_')[0]
-		hashed_email_base64 = wipe.hashed_email_base64()
-		new_name = wipe.s3.replace(email, hashed_email_base64)
-		file_exists = s3.exists(new_name)
-
-		if file_exists:
-			wipe.s3 = new_name
-			wipe.save()
-
-		response.append({
-			'email': email,
-			'hashed_email': wipe.hashed_email,
-			'new_file': new_name,
-			'file_exists': file_exists
-		})
-
-	return JsonResponse(response, safe=False)
--- midna/src/utils/s3.py
+++ midna/src/utils/s3.py
@@ -8,30 +8,6 @@ class S3:
 		self.s3 = client('s3')
 		self.bucket = environ.get('AWS_BUCKET')
 
-	def rename_file(self, old_name, new_name):
-		try:
-			copy_source = {
-				'Bucket': self.bucket,
-				'Key': old_name,
-			}
-
-			self.s3.copy_object(
-				Bucket = self.bucket,
-				CopySource = copy_source,
-				Key = new_name,
-			)
-
-			self.s3.delete_object(
-				Bucket = self.bucket,
-				Key = old_name,
-			)
-
-			return True
-
-		except ClientError as error:
-			print(error)
-			return False
-
 	def exists(self, name):
 		try:
 			self.s3.head_object(
