76fc0f73b6d8a9d7e711f1527ae3a6dc6a073b4e
Dara Keon <lc.sales.duarte@gmail.com>
2023-08-30 22:55:32 +0100

    android: remove kotlinx from tfa

--- android/App/src/main/kotlin/com/darakeon/dfm/tfa/TFAActivity.kt
+++ android/App/src/main/kotlin/com/darakeon/dfm/tfa/TFAActivity.kt
@@ -1,19 +1,19 @@
 package com.darakeon.dfm.tfa
 
 import android.view.View
-import com.darakeon.dfm.R
 import com.darakeon.dfm.accounts.AccountsActivity
 import com.darakeon.dfm.base.BaseActivity
 import com.darakeon.dfm.databinding.TfaBinding
 import com.darakeon.dfm.lib.extensions.redirect
-import kotlinx.android.synthetic.main.tfa.code
 
 class TFAActivity : BaseActivity<TfaBinding>() {
-	override val contentViewId = R.layout.tfa
+	override fun inflateBinding(): TfaBinding {
+		return TfaBinding.inflate(layoutInflater)
+	}
 
 	fun verify(@Suppress("UNUSED_PARAMETER") view: View) {
 		callApi {
-			it.validateTFA(code.text.toString()) {
+			it.validateTFA(binding.code.text.toString()) {
 				redirect<AccountsActivity>()
 			}
 		}
