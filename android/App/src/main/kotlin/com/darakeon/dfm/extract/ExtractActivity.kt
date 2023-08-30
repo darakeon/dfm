e6a83e4f5d356d4598267b6371b6dd96e87c751b
Dara Keon <lc.sales.duarte@gmail.com>
2023-08-30 23:32:28 +0100

    android: remove kotlinx from extract

--- android/App/src/main/kotlin/com/darakeon/dfm/extract/ExtractActivity.kt
+++ android/App/src/main/kotlin/com/darakeon/dfm/extract/ExtractActivity.kt
@@ -20,15 +20,15 @@ import com.darakeon.dfm.lib.extensions.refresh
 import com.darakeon.dfm.lib.extensions.setValueColored
 import com.darakeon.dfm.lib.extensions.swipe
 import com.darakeon.dfm.summary.SummaryActivity
-import kotlinx.android.synthetic.main.extract.empty_list
-import kotlinx.android.synthetic.main.extract.main
-import kotlinx.android.synthetic.main.extract.main_table
-import kotlinx.android.synthetic.main.extract.reportChange
-import kotlinx.android.synthetic.main.extract.total_title
-import kotlinx.android.synthetic.main.extract.total_value
 import java.util.Calendar
 
 class ExtractActivity : BaseActivity<ExtractBinding>() {
+	override fun inflateBinding(): ExtractBinding {
+		return ExtractBinding.inflate(layoutInflater)
+	}
+
+	override val title = R.string.title_activity_extract
+
 	private var accountUrl: String = ""
 
 	private val now = Calendar.getInstance()
@@ -40,11 +40,8 @@ class ExtractActivity : BaseActivity<ExtractBinding>() {
 	private var extract = Extract()
 	private val extractKey = "extract"
 
-	override val contentViewId = R.layout.extract
-	override val title = R.string.title_activity_extract
-
-	override val refresh: SwipeRefreshLayout?
-		get() = main
+	override val refresh: SwipeRefreshLayout
+		get() = binding.main
 
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
@@ -91,7 +88,7 @@ class ExtractActivity : BaseActivity<ExtractBinding>() {
 		val date = Calendar.getInstance()
 		date.set(Calendar.YEAR, year)
 		date.set(Calendar.MONTH, month)
-		reportChange.text = date.formatNoDay()
+		binding.reportChange.text = date.formatNoDay()
 	}
 
 	override fun onSaveInstanceState(outState: Bundle) {
@@ -150,21 +147,21 @@ class ExtractActivity : BaseActivity<ExtractBinding>() {
 	}
 
 	private fun fillMoves() {
-		total_title.text = extract.title
-		total_value.setValueColored(extract.total)
+		binding.totalTitle.text = extract.title
+		binding.totalValue.setValueColored(extract.total)
 
 		if (extract.moveList.isEmpty()) {
-			main_table.visibility = View.GONE
-			empty_list.visibility = View.VISIBLE
+			binding.mainTable.visibility = View.GONE
+			binding.emptyList.visibility = View.VISIBLE
 
-			main.listChild = main_table
-			main.swipe(Direction.Right, this::past)
-			main.swipe(Direction.Left, this::future)
+			binding.main.listChild = binding.mainTable
+			binding.main.swipe(Direction.Right, this::past)
+			binding.main.swipe(Direction.Left, this::future)
 		} else {
-			main_table.visibility = View.VISIBLE
-			empty_list.visibility = View.GONE
+			binding.mainTable.visibility = View.VISIBLE
+			binding.emptyList.visibility = View.GONE
 
-			main_table.adapter = MoveAdapter(
+			binding.mainTable.adapter = MoveAdapter(
 				this,
 				extract.moveList,
 				extract.canCheck,
@@ -174,8 +171,8 @@ class ExtractActivity : BaseActivity<ExtractBinding>() {
 				this::uncheckMove
 			)
 
-			main_table.swipe(Direction.Right, this::past)
-			main_table.swipe(Direction.Left, this::future)
+			binding.mainTable.swipe(Direction.Right, this::past)
+			binding.mainTable.swipe(Direction.Left, this::future)
 		}
 	}
 
