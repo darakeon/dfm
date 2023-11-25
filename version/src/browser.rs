2c03666dde203f95f504bec14b2fa87349057474
Dara Keon <lc.sales.duarte@gmail.com>
2023-11-25 19:27:56 +0000

    version: fix changing package lock

--- version/src/browser.rs
+++ version/src/browser.rs
@@ -3,16 +3,22 @@ use regex::Regex;
 use crate::file::{get_path, get_content, set_content};
 use crate::version::Version;
 
-fn path() -> String { get_path(vec!["..", "site", "Tests", "Browser", "package.json"]) }
+fn path_main() -> String { get_path(vec!["..", "site", "Tests", "Browser", "package.json"]) }
+fn path_lock() -> String { get_path(vec!["..", "site", "Tests", "Browser", "package-lock.json"]) }
 
 pub fn update_node(version: &Version) {
 	let old_version = config_version(&version.prev);
 	let new_version = config_version(&version.code);
 
-	let content = get_content(path())
+	let content_main = get_content(path_main())
 		.replace(&old_version, &new_version);
 
-	set_content(path(), content);
+	set_content(path_main(), content_main);
+
+	let content_lock = get_content(path_lock())
+		.replace(&old_version, &new_version);
+
+	set_content(path_lock(), content_lock);
 }
 
 fn config_version(version: &str) -> String {
