86e4fc533fc1e0f5de89813628616cd1636550f1
Dara Keon <lc.sales.duarte@gmail.com>
2023-12-01 14:32:27 +0000

    version: fix cargo lock version

--- version/src/rust.rs
+++ version/src/rust.rs
@@ -3,16 +3,22 @@ use regex::Regex;
 use crate::file::{get_path, get_content, set_content};
 use crate::version::Version;
 
-fn path() -> String { get_path(vec!["..", "version", "Cargo.toml"]) }
+fn path_toml() -> String { get_path(vec!["..", "version", "Cargo.toml"]) }
+fn path_lock() -> String { get_path(vec!["..", "version", "Cargo.lock"]) }
 
 pub fn update_rust(version: &Version) {
 	let old_version = config_version(&version.prev);
 	let new_version = config_version(&version.code);
 
-	let content = get_content(path())
+	let content_toml = get_content(path_toml())
 		.replace(&old_version, &new_version);
 
-	set_content(path(), content);
+	set_content(path_toml(), content_toml);
+
+	let content_lock = get_content(path_lock())
+		.replace(&old_version, &new_version);
+
+	set_content(path_lock(), content_lock);
 }
 
 fn config_version(version: &str) -> String {
