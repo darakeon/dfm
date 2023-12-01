a26831577edd935bb8bf0353765d3fbce19475b4
Dara Keon <lc.sales.duarte@gmail.com>
2023-12-01 04:18:16 +0000

    version: add dependabot file change

--- version/src/dependabot.rs
+++ version/src/dependabot.rs
@@ -1,13 +1,11 @@
-use regex::Regex;
-
 use crate::file::{get_path, get_content, set_content};
 use crate::version::Version;
 
 fn path() -> String { get_path(vec!["..", ".github", "dependabot.yml"]) }
 
-pub fn update_node(version: &Version) {
-	let old_version = config_version(&version.prev);
-	let new_version = config_version(&version.code);
+pub fn update_dependabot(version: &Version) {
+	let old_version = config_version(&version.code);
+	let new_version = config_version(&version.next);
 
 	let content = get_content(path())
 		.replace(&old_version, &new_version);
