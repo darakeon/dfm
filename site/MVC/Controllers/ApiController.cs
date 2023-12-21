11a7ac3cd7efdb797818c7f5335f9666d7e7c206
Dara Keon <lc.sales.duarte@gmail.com>
2023-12-21 16:33:31 +0000

    site: show error when try to access api via site

---
+++ site/MVC/Controllers/ApiController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using DFM.BusinessLogic.Exceptions;
+using DFM.Generic;
+using DFM.MVC.Helpers.Controllers;
+using DFM.MVC.Helpers.Extensions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DFM.MVC.Controllers
+{
+	[Wizard.Avoid]
+	public class ApiController : Controller
+	{
+		public IActionResult Index()
+		{
+			return Json(new Dictionary<String, Object>
+			{
+				{ "code", Error.UpdateApp },
+				{ "message", HttpContext.Translate("UpdateApp") + Cfg.Version },
+			});
+		}
+	}
+}
