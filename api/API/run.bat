529a4ac4decc22e25227abaa010e4333695b0b40
Dara Keon <lc.sales.duarte@gmail.com>
2023-12-21 16:51:02 +0000

    api: disambiguate ports

--- api/API/run.bat
+++ api/API/run.bat
@@ -1,5 +1,5 @@
 @echo off
 
 set ASPNETCORE_ENVIRONMENT=Development
-set ASPNETCORE_URLS=http://localhost:2409;http://0.0.0.0:2409
+set ASPNETCORE_URLS=http://localhost:2312;http://0.0.0.0:2312
 bin\DFM.API.exe
