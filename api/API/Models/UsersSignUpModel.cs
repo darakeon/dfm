2d3f18917440f641d2c8a855199bc50de2a41054
Dara Keon <lc.sales.duarte@gmail.com>
2024-02-18 23:38:54 +0000

    auth: add signup to API

---
+++ api/API/Models/UsersSignUpModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using DFM.BusinessLogic.Response;
+
+namespace DFM.API.Models
+{
+    public class UsersSignUpModel : BaseApiModel
+    {
+        [Required]
+        public String Email { get; set; }
+
+        [Required]
+        public String Password { get; set; }
+
+		[Required]
+        public Boolean AcceptedContract { get; set; }
+
+        [Required]
+        public String Language { get; set; }
+
+        [Required]
+        public String Timezone { get; set; }
+
+		internal void SignUp()
+        {
+	        auth.SaveUser(
+		        new SignUpInfo
+		        {
+			        Email = Email,
+			        Password = Password,
+			        AcceptedContract = AcceptedContract,
+			        Language = Language,
+			        TimeZone = Timezone,
+			        RetypePassword = Password,
+		        }
+		    );
+        }
+	}
+}
