9223f354ea6f56259a6f88f87b79d533a197d82c
Dara Keon <lc.sales.duarte@gmail.com>
2024-03-11 02:17:05 +0000

    api: return code and text for enums

---
+++ api/API/Starters/Serialization.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using JetBrains.Annotations;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace DFM.API.Starters
+{
+	public class Serialization : DefaultContractResolver
+	{
+		public static void Set(MvcNewtonsoftJsonOptions options)
+		{
+			options.SerializerSettings.ContractResolver = new Serialization();
+		}
+
+		public Serialization()
+		{
+			NamingStrategy = new KebabCaseNamingStrategy();
+		}
+
+		protected override JsonPrimitiveContract CreatePrimitiveContract(Type type)
+		{
+			var realType = getRealType(type);
+
+			if (realType.IsEnum)
+				return new EnumContract(type, realType);
+
+			return base.CreatePrimitiveContract(type);
+		}
+
+		private static Type getRealType(Type type)
+		{
+			var isNullable =
+				type.IsGenericType
+				&& type.Name == typeof(Int32?).Name
+				&& type.GenericTypeArguments.Length == 1;
+
+			return isNullable 
+				? type.GenericTypeArguments[0] 
+				: type;
+		}
+
+		class EnumContract : JsonPrimitiveContract
+		{
+			public EnumContract([NotNull] Type type, Type realType) : base(type)
+			{
+				Converter = new EnumConverter(realType);
+			}
+		}
+
+		class EnumConverter(Type realType) : JsonConverter
+		{
+			public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+			{
+				if (value == null)
+				{
+					serializer.Serialize(writer, null);
+				}
+				else
+				{
+					serializer.Serialize(writer, new
+					{
+						code = (int)value,
+						text = kebabCase(value.ToString()),
+					});
+				}
+			}
+
+			private String kebabCase(String text)
+			{
+				var regex = new Regex("([A-Z])");
+				text = regex.Replace(text, "-$1");
+				if (text.StartsWith("-"))
+					text = text.Substring(1);
+				return text.ToLower();
+			}
+
+			public override object ReadJson(JsonReader reader, Type type, object existingValue, JsonSerializer serializer)
+			{
+				var value = serializer.Deserialize<String>(reader);
+
+				if (value == null) return null;
+
+				value = value.Replace("-", "");
+
+				Enum.TryParse(realType, value, true, out var result);
+
+				return result;
+			}
+
+			public override bool CanConvert(Type objectType)
+			{
+				return objectType.IsEnum;
+			}
+		}
+
+
+	}
+}
