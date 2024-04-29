1cf6eaf31d8b2ced47d1bed8627cb482d1f89a01
Dara Keon <lc.sales.duarte@gmail.com>
2024-04-29 22:37:57 +0100

    tests: add scenario number adjustment to fixer

--- core/Tests/BusinessLogic/fix_test_codes.py
+++ core/Tests/BusinessLogic/fix_test_codes.py
@@ -39,15 +39,20 @@ for d, test_dir_name in enumerate(test_dir_names):
 			print(f'Wrong first line [{dir_letter}{file_letter}]: {lines[0]}')
 			lines[0] = lines[0][0:9] + dir_letter + file_letter + lines[0][11:]
 
+		test_number = 1
+
 		for l, line in enumerate(lines):
 			lines[l] = lines[l].replace('\r', '')
 
 			if not match(f'^Scenario:', line):
 				continue
 
-			if not match(f'^Scenario: {dir_letter}{file_letter}\d\d.', line):
+			scenario_prefix = f'Scenario: {dir_letter}{file_letter}{test_number:02}. '
+			if not match(f'^{scenario_prefix}', line):
 				print(f'Wrong scenario line [{dir_letter}{file_letter}]: {lines[l]}')
-				lines[l] = lines[l][0:10] + dir_letter + file_letter + lines[l][12:]
+				lines[l] = scenario_prefix + lines[l][16:]
+
+			test_number = test_number + 1
 		#match()
 
 		test_file.close()
