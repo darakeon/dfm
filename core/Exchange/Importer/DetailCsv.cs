0633a92a8347221dcb7ee865eda53c4f6d0d9756
Dara Keon <lc.sales.duarte@gmail.com>
2024-05-06 22:59:52 +0100

    WIP money: moves file import

---
+++ core/Exchange/Importer/DetailCsv.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DFM.Exchange.Importer
+{
+	public class DetailCsv
+	{
+		public String Description { get; set; }
+		public String Amount { get; set; }
+		public String Value { get; set; }
+		public String Conversion { get; set; }
+	}
+}
