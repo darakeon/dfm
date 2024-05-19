36f8af2bd96a9ca84e8f04c9d5c9c45d86a78025
Dara Keon <lc.sales.duarte@gmail.com>
2024-05-19 14:46:10 +0100

    money: refactor move validation

---
+++ core/BusinessLogic/Validators/ScheduleValidator.cs
@@ -0,0 +1,10 @@
+﻿using DFM.BusinessLogic.Exceptions;
+using DFM.Entities;
+using DFM.Entities.Bases;
+
+namespace DFM.BusinessLogic.Validators;
+
+public class ScheduleValidator() : GenericMoveValidator<Schedule>(
+	MaxLen.ScheduleDescription,
+	Error.TooLargeScheduleDescription
+);
