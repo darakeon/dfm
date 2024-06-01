7482ae8cbac44b14cbeedcd689bc9e8a8f2cc6af
Dara Keon <lc.sales.duarte@gmail.com>
2024-06-01 21:19:38 +0100

    money: return errors by line

---
+++ core/BusinessLogic/Exceptions/ErrorWithMetadata.cs
@@ -0,0 +1,7 @@
+﻿namespace DFM.BusinessLogic.Exceptions;
+
+public class ErrorWithMetadata(Error error, object metadata = null)
+{
+	public Error Error { get; } = error;
+	public object Metadata { get; } = metadata;
+}
