970a859dfd97ff6be5646f703542dc8f9936cc9a
Dara Keon <lc.sales.duarte@gmail.com>
2024-06-07 23:15:42 +0100

    robot: add original file name to archive
    To easily recognize origin of data

--- core/Entities/Archive.cs
+++ core/Entities/Archive.cs
@@ -9,6 +9,8 @@ public class Archive : IEntityLong
 {
 	public virtual Int64 ID { get; set; }
 
+	public virtual String Filename { get; set; }
+
 	public virtual ImportStatus Status { get; set; }
 
 	public virtual User User { get; set; }
