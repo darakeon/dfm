05d9082b897aed9522bf2f1b5c7addf5f79b0c89
Dara Keon <lc.sales.duarte@gmail.com>
2024-06-12 01:16:35 +0100

    chore: remove old prototype

--- proto-file-exchange/Properties/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("DFM.FileExchange")]
-[assembly: AssemblyProduct("DFM.FileExchange")]
-[assembly: Guid("bbcd6745-b50c-47c2-bab5-c6b5a362404c")]
