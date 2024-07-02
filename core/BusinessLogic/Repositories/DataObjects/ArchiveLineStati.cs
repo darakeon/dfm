d6293eb862a67b64df7b774bd172cda3e2488cb5
Dara Keon <lc.sales.duarte@gmail.com>
2024-07-02 13:44:22 +0100

    robot: finish archives

---
+++ core/BusinessLogic/Repositories/DataObjects/ArchiveLineStati.cs
@@ -0,0 +1,11 @@
+﻿using DFM.Entities;
+using DFM.Entities.Enums;
+
+namespace DFM.BusinessLogic.Repositories.DataObjects
+{
+	internal class ArchiveLineStati
+	{
+		public Archive Archive { get; set; }
+		public ImportStatus LineStati { get; set; }
+	}
+}
