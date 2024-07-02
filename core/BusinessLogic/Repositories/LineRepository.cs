1ff70dc34e7ff88a234b3181a5ce26cc4433a698
Dara Keon <lc.sales.duarte@gmail.com>
2024-07-02 13:44:22 +0100

    robot: finish archives

--- core/BusinessLogic/Repositories/LineRepository.cs
+++ core/BusinessLogic/Repositories/LineRepository.cs
@@ -1,7 +1,21 @@
-﻿using DFM.Entities;
+﻿using System.Collections.Generic;
+using DFM.BusinessLogic.Repositories.DataObjects;
+using DFM.Entities;
+using DFM.Entities.Enums;
 
 namespace DFM.BusinessLogic.Repositories;
 
 internal class LineRepository : Repo<Line>
 {
+	public IList<ArchiveLineStati> GetArchivesPending()
+	{
+		return NewQuery()
+			.LeftJoin(l => l.Archive)
+			.Where(l => l.Archive.Status == ImportStatus.Pending)
+			.TransformResult<ArchiveLineStati>()
+			.GroupBy(l => l.Archive, als => als.Archive)
+			.Max(l => l.Status, als => als.MaxLineStati)
+			.Min(l => l.Status, als => als.MinLineStati)
+			.List;
+	}
 }
