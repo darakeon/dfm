057cbdfd3b896b2b50b35f4fb0d5d13a8d053c28
Dara Keon <lc.sales.duarte@gmail.com>
2024-08-09 01:02:53 +0100

    site: add archives pages

---
+++ site/MVC/Models/ArchivesUploadModel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using DFM.BusinessLogic.Exceptions;
+using Microsoft.AspNetCore.Http;
+
+namespace DFM.MVC.Models;
+
+public class ArchivesUploadModel : BaseSiteModel
+{
+	[Required, FileExtensions(
+		 Extensions = "csv", 
+		 ErrorMessage = "*"
+	)]
+	public IFormFile File { get; set; }
+
+	public async Task<IList<String>> SaveArchive()
+	{
+		var errors = new List<String>();
+
+		try
+		{
+			var filename = File.FileName;
+
+			await using var stream = File.OpenReadStream();
+			using var reader = new StreamReader(stream);
+
+			var content = await reader.ReadToEndAsync();
+
+			robot.ImportMovesFile(filename, content);
+		}
+		catch (CoreError error)
+		{
+			error.Types
+				.OrderBy(e => e.Metadata)
+				.ToList()
+				.ForEach(
+					e => errors.Add($"{e.Metadata}: {translator[e.Error]}")
+				);
+		}
+
+		return errors;
+	}
+}
