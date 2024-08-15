14349bd622cd27c98477ee1eb2aee7bcba9daa41
Dara Keon <lc.sales.duarte@gmail.com>
2024-08-15 19:27:08 +0100

    site: add button for asset

--- site/MVC/Helpers/Views/Button.cs
+++ site/MVC/Helpers/Views/Button.cs
@@ -8,26 +8,38 @@ namespace DFM.MVC.Helpers.Views
 		public String Text { get; }
 		public String Action { get; }
 		public String Controller { get; }
+		public String Asset { get; }
 		public String Class { get; }
 		public Boolean PullLeft { get; }
 
-		public Button(String text, [AspMvcAction] String action, Boolean pullLeft = false)
+		private Button(String text, [AspMvcAction] String action, [AspMvcController] String controller, String asset, String @class, Boolean pullLeft = false)
 		{
 			Text = text;
 			Action = action;
+			Controller = controller;
+			Asset = asset;
+			Class = @class;
 			PullLeft = pullLeft;
 		}
 
-		public Button(String text, [AspMvcAction] String action, String @class, Boolean pullLeft = false)
-			: this(text, action, pullLeft)
+		public static Button ForAction(String text, [AspMvcAction] String action, Boolean pullLeft = false)
 		{
-			Class = @class;
+			return ForAction(text, action, null, pullLeft);
 		}
 
-		public Button(String text, [AspMvcController] String controller, [AspMvcAction] String action, String @class, Boolean pullLeft = false)
-			: this(text, action, @class, pullLeft)
+		public static Button ForAction(String text, [AspMvcAction] String action, String @class, Boolean pullLeft = false)
 		{
-			Controller = controller;
+			return ForAction(text, action, null, @class, pullLeft);
+		}
+
+		public static Button ForAction(String text, [AspMvcAction] String action, [AspMvcController] String controller, String @class, Boolean pullLeft = false)
+		{
+			return new Button(text, action, controller, null, @class, pullLeft);
+		}
+
+		public static Button ForAsset(String text, String asset, String @class, Boolean pullLeft = false)
+		{
+			return new Button(text, null, null, asset, @class, pullLeft);
 		}
 	}
 }
