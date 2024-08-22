766609e2bfea1dcef458e1833c4922a03628b397
Dara Keon <lc.sales.duarte@gmail.com>
2024-08-23 00:30:27 +0100

    site: add cancel archive

---
+++ site/MVC/Models/ArchiveRowModel.cs
@@ -0,0 +1,15 @@
+﻿using DFM.BusinessLogic.Response;
+using DFM.MVC.Helpers.Views;
+
+namespace DFM.MVC.Models;
+
+public class ArchiveRowModel(
+	ArchiveInfo archive,
+	WizardHL wizardHL = null
+)
+{
+	public ArchiveInfo Archive { get; } = archive;
+
+	# nullable enable
+	public WizardHL? WizardHL { get; } = wizardHL;
+}
--- site/MVC/Models/ArchivesIndexModel.cs
+++ site/MVC/Models/ArchivesIndexModel.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using DFM.BusinessLogic.Response;
 
 namespace DFM.MVC.Models;
@@ -7,4 +8,12 @@ public class ArchivesIndexModel : BaseSiteModel
 {
 	public IList<ArchiveInfo> ArchiveList { get; set; }
 		= service.Robot.GetArchiveList();
+
+	public void Cancel(Guid id)
+	{
+		var archive = service.Robot.CancelArchive(id);
+		Archive = new ArchiveRowModel(archive);
+	}
+
+	public ArchiveRowModel Archive { get; private set; }
 }
