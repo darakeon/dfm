d0927e6ce4faacaa705b80eee51434b468d8004c
Dara Keon <lc.sales.duarte@gmail.com>
2024-08-25 02:53:09 +0100

    WIP robot: order export

---
+++ core/Entities/Order.cs
@@ -0,0 +1,26 @@
+﻿using Keon.Util.DB;
+using System;
+using System.Collections.Generic;
+
+namespace DFM.Entities
+{
+	public class Order : IEntityLong
+	{
+		public virtual Int64 ID { get; set; }
+		public virtual Byte[] ExternalId { get; set; }
+
+		public virtual DateTime Start { get; set; }
+		public virtual DateTime End { get; set; }
+
+		public virtual User User { get; set; }
+
+		public virtual IList<Account> AccountList { get; set; }
+		public virtual IList<Category> CategoryList { get; set; }
+
+		public virtual Guid Guid
+		{
+			get => new(ExternalId);
+			set => ExternalId = value.ToByteArray();
+		}
+	}
+}
