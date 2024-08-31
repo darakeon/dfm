841953d2a1000a8f60b1a472aec884757ffac4a0
Dara Keon <lc.sales.duarte@gmail.com>
2024-08-31 13:10:45 +0100

    core: move wipe days count to variable

---
+++ core/Entities/Bases/DayLimits.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DFM.Entities.Bases
+{
+	public static class DayLimits
+	{
+		public const Int32 WIPE_WARN_1 = 30;
+		public const Int32 WIPE_WARN_2 = 60;
+		public const Int32 WIPE = 90;
+
+
+	}
+}
