5c29c7bab575dd414f4be455752a44239b3fb42b
Dara Keon <lc.sales.duarte@gmail.com>
2024-10-07 04:09:34 +0100

    core: add plan entity

---
+++ core/BusinessLogic/Repositories/Mappings/PlanMap.cs
@@ -0,0 +1,18 @@
+﻿using DFM.Entities;
+using DFM.Entities.Bases;
+using FluentNHibernate.Automapping;
+using FluentNHibernate.Automapping.Alterations;
+
+namespace DFM.BusinessLogic.Repositories.Mappings;
+
+// ReSharper disable once UnusedMember.Global
+public class PlanMap : IAutoMappingOverride<Plan>
+{
+	public void Override(AutoMapping<Plan> mapping)
+	{
+		mapping.Map(l => l.Name)
+			.Length(MaxLen.PlanName);
+
+		mapping.IgnoreProperty(l => l.Price);
+	}
+}
