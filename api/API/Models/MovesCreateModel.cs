354081a3e20182d8d92ea72406cf632ba74f1ceb
Dara Keon <lc.sales.duarte@gmail.com>
2024-10-11 00:48:56 +0100

    api: add details limit

--- api/API/Models/MovesCreateModel.cs
+++ api/API/Models/MovesCreateModel.cs
@@ -15,7 +15,7 @@ namespace DFM.API.Models
 			Move = money.GetMove(guid);
 		}
 
-		public bool IsUsingCategories { get; }
+		public Boolean IsUsingCategories { get; }
 		public MoveInfo Move { get; set; }
 
 		public void Save(MoveInfo info)
