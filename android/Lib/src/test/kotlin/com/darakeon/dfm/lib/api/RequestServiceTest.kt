9b25784e2316f6314a6455dfbb5e8844ae275337
Dara Keon <lc.sales.duarte@gmail.com>
2024-10-11 01:30:57 +0100

    android: read plan limit detail from api

--- android/Lib/src/test/kotlin/com/darakeon/dfm/lib/api/RequestServiceTest.kt
+++ android/Lib/src/test/kotlin/com/darakeon/dfm/lib/api/RequestServiceTest.kt
@@ -238,6 +238,7 @@ class RequestServiceTest: BaseTest() {
 
 		val data = body.data!!
 		assertThat(data.isUsingCategories, `is`(true))
+		assertThat(data.planLimitDetailByParent, `is`(3))
 
 		val accountList = data.accountList
 		assertThat(accountList.size, `is`(2))
