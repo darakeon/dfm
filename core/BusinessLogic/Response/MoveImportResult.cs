bcd30a866197d572e9874f8bd13922426df802c2
Dara Keon <lc.sales.duarte@gmail.com>
2024-10-22 03:26:57 +0100

    executor: encapsulate import error on response
    Executor is a service that should not abort all the work because of one
    failure

---
+++ core/BusinessLogic/Response/MoveImportResult.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using DFM.BusinessLogic.Exceptions;
+using DFM.Entities;
+
+namespace DFM.BusinessLogic.Response;
+
+public class MoveImportResult
+{
+	internal MoveImportResult(User user, MoveResult result)
+		: this(user, true)
+	{
+		Move = result;
+	}
+
+	internal MoveImportResult(User user, CoreError error)
+		: this(user, false)
+	{
+		Error = error;
+	}
+
+	private MoveImportResult(User user, Boolean success)
+	{
+		User = user;
+		Success = success;
+	}
+
+	public User User { get; }
+	public Boolean Success { get; }
+	public MoveResult Move { get; }
+	public CoreError Error { get; }
+}
--- docs/todo/system-limits.md
+++ docs/todo/system-limits.md
@@ -9,7 +9,7 @@
 - [x] Add Order export Moves qty limit - order / export
 - [x] Add Schedule active limit - schedule
 - [x] Handle Schedule error by Move/Month limit at Robot - the error is logged and one user does not keep other user from run schedules
-- [ ] Handle Import error by Move/Month limit at Robot
+- [x] Handle Import error by Move/Month limit at Robot
 - [ ] Handle Export error by Move/Order limit at Robot
 - [ ] Show Schedule error by Move/Month limit at Site
 - [ ] Show Import error by Move/Month limit at Site
