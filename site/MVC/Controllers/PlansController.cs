2f69f7f2073f68f203cbf33f23fc267f323cc1be
Dara Keon <lc.sales.duarte@gmail.com>
2024-11-16 01:42:37 +0000

    site: add usage limit screen

---
+++ site/MVC/Controllers/PlansController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using DFM.MVC.Helpers.Authorize;
+using DFM.MVC.Helpers.Controllers;
+using DFM.MVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DFM.MVC.Controllers;
+
+[Auth]
+public class PlansController : BaseController
+{
+	[HttpGetAndHead]
+	public IActionResult Index()
+	{
+		return View(new PlansIndexModel());
+	}
+}
---
+++ site/MVC/Models/PlansIndexModel.cs
@@ -0,0 +1,8 @@
+﻿using DFM.Entities;
+
+namespace DFM.MVC.Models;
+
+public class PlansIndexModel : BaseSiteModel
+{
+	public Plan Plan { get; set; } = law.GetPlan();
+}
