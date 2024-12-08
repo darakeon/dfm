cc080b768bc797b304190d8503e6b4e69fd97c14
Dara Keon <lc.sales.duarte@gmail.com>
2024-12-08 14:19:55 +0000

    site: hide tips for ops pages

--- site/MVC/Models/OpsCodeModel.cs
+++ site/MVC/Models/OpsCodeModel.cs
@@ -1,9 +1,12 @@
-﻿using DFM.Email;
+﻿using System;
+using DFM.Email;
 
 namespace DFM.MVC.Models
 {
 	public class OpsCodeModel : BaseSiteModel
 	{
 		public Error.Status EmailSent { get; set; }
+
+		protected override Boolean ShowTip => false;
 	}
 }
