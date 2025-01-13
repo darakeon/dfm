6f1450df780ca18ca0ee47c1d469cde413007298
Dara Keon <lc.sales.duarte@gmail.com>
2025-01-13 21:51:47 +0000

    struct: add maintenance for api

---
+++ version/src/maintenance.rs
@@ -0,0 +1,13 @@
+use regex::Regex;
+
+use crate::file::{get_path, get_content, set_content};
+use crate::version::Version;
+
+fn path_api() -> String { get_path(vec!["..", "architecture", "maintenance", "api", "index.json"]) }
+
+pub fn update_maintenance_api_json(version: &Version) {
+	let content_api = get_content(path_api())
+		.replace(&version.prev, &version.code);
+
+	set_content(path_api(), content_api);
+}
