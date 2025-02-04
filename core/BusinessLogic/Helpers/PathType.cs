505c63fb0450799b3224287ce6983a7a1f84b89c
Dara Keon <lc.sales.duarte@gmail.com>
2025-02-04 20:55:14 +0000

    chore: refactor enum

--- core/BusinessLogic/Helpers/PathType.cs
+++ core/BusinessLogic/Helpers/PathType.cs
@@ -2,11 +2,11 @@ namespace DFM.BusinessLogic.Helpers
 {
 	public enum PathType
 	{
-		None,
-		UserVerification,
-		PasswordReset,
-		UnsubscribeMoveMail,
-		DisableToken,
-		DeleteCsvData,
+		None = 0,
+		UserVerification = 1,
+		PasswordReset = 2,
+		UnsubscribeMoveMail = 3,
+		DeleteCsvData = 4,
+		DisableToken = 999,
 	}
 }
