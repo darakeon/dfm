4da177596fad9b912325845481d7eda574599e86
Dara Keon <lc.sales.duarte@gmail.com>
2025-02-04 22:19:46 +0000

    struct: move validations to validator

---
+++ core/BusinessLogic/Validators/UserValidator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using DFM.BusinessLogic.Exceptions;
+using DFM.Entities;
+using Keon.Util.Crypto;
+
+namespace DFM.BusinessLogic.Validators;
+
+internal class UserValidator
+{
+	public Boolean VerifyPassword(User user, String password)
+	{
+		return password != null
+			&& Crypt.Check(password, user.Password);
+	}
+
+	public void CheckPassword(User user, String password)
+	{
+		if (!VerifyPassword(user, password))
+			throw Error.WrongPassword.Throw();
+	}
+
+	public void CheckUserDeletion(User user)
+	{
+		if (user.Control.ProcessingDeletion)
+			throw Error.UserDeleted.Throw();
+
+		if (user.Control.WipeRequest != null)
+			throw Error.UserAskedWipe.Throw();
+	}
+}
