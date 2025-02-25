7533bb8afc578e8b98b3327ba70d40febad08b80
Dara Keon <lc.sales.duarte@gmail.com>
2025-02-25 23:20:55 +0000

    core: add dictionary extension
    Better try get value

---
+++ core/Generic/DictionaryExtension.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace DFM.Generic
+{
+	public static class DictionaryExtension
+	{
+		public static TValue TryGetValue<TKey, TValue>(
+			this IDictionary<TKey, TValue> dictionary,
+			TKey key, TValue defaultValue = default
+		)
+		{
+			return dictionary.TryGetValue(key, out var value)
+				? value
+				: defaultValue;
+		}
+	}
+}
