6bb3f1437b854d422d1cf37530d0e092e85b4356
Dara Keon <lc.sales.duarte@gmail.com>
2025-02-28 16:12:48 +0000

    chore: standarize quote

--- site/Tests/Browser/jest-puppeteer.config.js
+++ site/Tests/Browser/jest-puppeteer.config.js
@@ -3,7 +3,7 @@ const args = []
 let browser = '/usr/bin/google-chrome'
 
 if (process.getuid && process.getuid() == 0) {
-	args.push("--no-sandbox")
+	args.push('--no-sandbox')
 } else if (process.platform.startsWith('win')) {
 	browser = 'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe'
 }
