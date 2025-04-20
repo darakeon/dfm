9ae70e91e9803c2416f06570ec5f25b8306456f6
Dara Keon <lc.sales.duarte@gmail.com>
2025-04-20 02:27:21 +0100

    core: improve dictionary extension

--- core/Tests/Generic/DictionaryExtensionTests.cs
+++ core/Tests/Generic/DictionaryExtensionTests.cs
@@ -2,47 +2,89 @@ using System;
 using System.Collections.Generic;
 using NUnit.Framework;
 
-namespace DFM.Generic.Tests
+namespace DFM.Generic.Tests;
+
+public class DictionaryExtensionTests
 {
-	public class DictionaryExtensionTests
+	[Test]
+	public void TryGetValue_Existent()
 	{
-		[Test]
-		public void TryGetValue_Existent()
+		var dictionary = new Dictionary<String, String>
 		{
-			var dictionary = new Dictionary<String, String>
-			{
-				{ "Hey", "listen" }
-			};
+			{ "Hey", "listen" }
+		};
 
-			var value = dictionary.TryGetValue("Hey", "navi");
+		var value = dictionary.TryGetValue("Hey", "navi");
 
-			Assert.That(value, Is.EqualTo("listen"));
-		}
+		Assert.That(value, Is.EqualTo("listen"));
+	}
 
-		[Test]
-		public void TryGetValue_NotExistent()
+	[Test]
+	public void TryGetValue_NotExistent()
+	{
+		var dictionary = new Dictionary<String, String>
 		{
-			var dictionary = new Dictionary<String, String>
-			{
-				{ "Hey", "listen" }
-			};
+			{ "Hey", "listen" }
+		};
 
-			var value = dictionary.TryGetValue("hey", "navi");
+		var value = dictionary.TryGetValue("hey", "navi");
 
-			Assert.That(value, Is.EqualTo("navi"));
-		}
+		Assert.That(value, Is.EqualTo("navi"));
+	}
 
-		[Test]
-		public void TryGetValue_NoDefault()
+	[Test]
+	public void TryGetValue_NoDefault()
+	{
+		var dictionary = new Dictionary<String, String>
 		{
-			var dictionary = new Dictionary<String, String>
-			{
-				{ "Hey", "listen" }
-			};
+			{ "Hey", "listen" }
+		};
 
-			var value = dictionary.TryGetValue("hey");
+		var value = dictionary.TryGetValue("hey");
 
-			Assert.That(value, Is.Null);
-		}
+		Assert.That(value, Is.Null);
+	}
+
+	[Test]
+	public void TryGetValueOrAdd_Existent()
+	{
+		var dictionary = new Dictionary<String, String>
+		{
+			{ "Hey", "listen" }
+		};
+
+		var value = dictionary.TryGetValueOrAdd("Hey", "navi");
+
+		Assert.That(value, Is.EqualTo("listen"));
+	}
+
+	[Test]
+	public void TryGetValueOrAdd_NotExistent()
+	{
+		var dictionary = new Dictionary<String, String>
+		{
+			{ "Hey", "listen" }
+		};
+
+		var value = dictionary.TryGetValueOrAdd("hey", "navi");
+
+		Assert.That(value, Is.EqualTo("navi"));
+		Assert.That(dictionary, Contains.Key("hey"));
+		Assert.That(dictionary["hey"], Is.EqualTo("navi"));
+	}
+
+	[Test]
+	public void TryGetValueOrAdd_NoDefault()
+	{
+		var dictionary = new Dictionary<String, String>
+		{
+			{ "Hey", "listen" }
+		};
+
+		var value = dictionary.TryGetValueOrAdd("hey");
+
+		Assert.That(value, Is.Null);
+		Assert.That(dictionary, Contains.Key("hey"));
+		Assert.That(dictionary["hey"], Is.Null);
 	}
 }
