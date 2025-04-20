45163c1a823716586385a14689195564fb26a47a
Dara Keon <lc.sales.duarte@gmail.com>
2025-04-20 02:27:21 +0100

    core: improve dictionary extension

--- core/Generic/DictionaryExtension.cs
+++ core/Generic/DictionaryExtension.cs
@@ -1,17 +1,29 @@
 ﻿using System.Collections.Generic;
 
-namespace DFM.Generic
+namespace DFM.Generic;
+
+public static class DictionaryExtension
 {
-	public static class DictionaryExtension
+	public static TValue TryGetValue<TKey, TValue>(
+		this IDictionary<TKey, TValue> dictionary,
+		TKey key, TValue defaultValue = default
+	)
 	{
-		public static TValue TryGetValue<TKey, TValue>(
-			this IDictionary<TKey, TValue> dictionary,
-			TKey key, TValue defaultValue = default
-		)
-		{
-			return dictionary.TryGetValue(key, out var value)
-				? value
-				: defaultValue;
-		}
+		return dictionary.TryGetValue(key, out var value)
+			? value
+			: defaultValue;
+	}
+
+	public static TValue TryGetValueOrAdd<TKey, TValue>(
+		this IDictionary<TKey, TValue> dictionary,
+		TKey key, TValue defaultValue = default
+	)
+	{
+		if (dictionary.TryGetValue(key, out var value))
+			return value;
+
+		dictionary.Add(key, defaultValue);
+
+		return defaultValue;
 	}
 }
