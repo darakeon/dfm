4fe7eb458ee54396787f187cd8e188065f3a26ed
Dara Keon <lc.sales.duarte@gmail.com>
2025-04-21 02:08:45 +0100

    chore: remove unused stuff

--- site/MVC/Controllers/PlansController.cs
+++ site/MVC/Controllers/PlansController.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using DFM.MVC.Helpers.Authorize;
+﻿using DFM.MVC.Helpers.Authorize;
 using DFM.MVC.Helpers.Controllers;
 using DFM.MVC.Models;
 using Microsoft.AspNetCore.Mvc;
--- site/MVC/Helpers/Global/ErrorManager.cs
+++ site/MVC/Helpers/Global/ErrorManager.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
-using DFM.BusinessLogic;
 using Keon.MVC.Cookies;
 using DFM.Email;
 using DFM.Logs;
