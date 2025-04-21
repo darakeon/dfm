7d480849bf21f02defdfa38589c6afffc2f0c631
Dara Keon <lc.sales.duarte@gmail.com>
2025-04-21 01:22:18 +0100

    logs: add factory

---
+++ core/Logs/LogFactory.cs
@@ -0,0 +1,14 @@
+﻿using DFM.Generic;
+
+namespace DFM.Logs
+{
+	public static class LogFactory
+	{
+		private static ILogService? logService;
+
+		public static ILogService Service =>
+			logService ??= Cfg.Log.Local
+				? new LocalLogService()
+				: new CloudWatchService();
+	}
+}
