1f1324d4615354ab0c8c1e3cf192d026a83b3efc
Dara Keon <lc.sales.duarte@gmail.com>
2025-04-25 19:46:26 +0100

    site: fix initialization process order

--- site/MVC/Starters/Settings.cs
+++ site/MVC/Starters/Settings.cs
@@ -1,13 +1,12 @@
 ﻿using System.IO;
 using DFM.Generic;
-using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 
 namespace DFM.MVC.Starters
 {
 	class Settings
 	{
-		public static void Initialize(IWebHostEnvironment env)
+		public static void Initialize(IHostEnvironment env)
 		{
 			Directory.SetCurrentDirectory(env.ContentRootPath);
 
