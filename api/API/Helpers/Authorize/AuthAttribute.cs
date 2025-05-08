f4b53d022ae3401619dc162a8d633e8f4c848fd6
Dara Keon <lc.sales.duarte@gmail.com>
2025-05-08 02:45:03 +0100

    core: move common class to base web

--- api/API/Helpers/Authorize/AuthAttribute.cs
+++ api/API/Helpers/Authorize/AuthAttribute.cs
@@ -1,65 +1,21 @@
-﻿using System;
-using DFM.API.Controllers;
-using DFM.BaseWeb.Helpers;
+﻿using DFM.API.Controllers;
 using DFM.BaseWeb.Helpers.Authorize;
-using DFM.BaseWeb.Helpers.Extensions;
-using DFM.BusinessLogic;
 using DFM.BusinessLogic.Exceptions;
-using DFM.Generic.Settings;
 using Microsoft.AspNetCore.Mvc.Filters;
 
 namespace DFM.API.Helpers.Authorize
 {
-	public class AuthAttribute : Attribute, IAuthorizationFilter
+	public class AuthAttribute : BaseAuthAttribute
 	{
-		private readonly AuthParams mandatory;
-
 		public AuthAttribute(AuthParams mandatory = AuthParams.None)
+			: base(mandatory | AuthParams.Mobile) { }
+
+		protected override void makeResult(
+			AuthorizationFilterContext context, Error error
+		)
 		{
-			this.mandatory = mandatory | AuthParams.Mobile;
-		}
-
-		private Service service;
-		private Current current => service.Current;
-		private ServiceAccess access => service.Access;
-		private Boolean isAuthenticated => current.IsAuthenticated;
-
-		private Boolean denyByAdmin =>
-			mandatory.HasFlag(AuthParams.Admin)
-				&& !current.IsAdm;
-
-		private Boolean denyByContract =>
-			!mandatory.HasFlag(AuthParams.IgnoreContract)
-				&& !access.Law.IsLastContractAccepted();
-		
-		private Boolean denyByTFA =>
-			!mandatory.HasFlag(AuthParams.IgnoreTFA)
-				&& !access.Auth.VerifyTicketTFA();
-		
-		private Boolean denyByMobile =>
-			!access.Auth.VerifyTicketType(TicketType.Mobile);
-
-		public void OnAuthorization(AuthorizationFilterContext context)
-		{
-			service = context.HttpContext.GetService();
-
-			if (!isAuthenticated)
-				makeResult(context, Error.LoginRequested);
-
-			else if (denyByTFA)
-				makeResult(context, Error.TFANotVerified);
-
-			else if (denyByContract)
-				makeResult(context, Error.NotSignedLastContract);
-
-			else if (denyByAdmin || denyByMobile)
-				makeResult(context, Error.Uninvited);
-		}
-
-		protected void makeResult(AuthorizationFilterContext filterContext, Error error)
-		{
-			filterContext.Result = BaseApiController.ApiError(
-				filterContext.HttpContext, error
+			context.Result = BaseApiController.ApiError(
+				context.HttpContext, error
 			);
 		}
 	}
---
+++ core/BaseWeb/Helpers/Authorize/BaseAuthAttribute.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using DFM.Generic.Settings;
+using DFM.BaseWeb.Helpers.Extensions;
+using DFM.BusinessLogic.Exceptions;
+
+namespace DFM.BaseWeb.Helpers.Authorize
+{
+	public abstract class BaseAuthAttribute(AuthParams mandatory)
+		: Attribute, IAuthorizationFilter
+	{
+		public void OnAuthorization(AuthorizationFilterContext context)
+		{
+			var service = context.HttpContext.GetService();
+			var current = service.Current;
+			var access = service.Access;
+			var isAuthenticated = current.IsAuthenticated;
+
+
+			var denyByAdmin =
+				isAuthenticated
+				&& mandatory.HasFlag(AuthParams.Admin)
+				&& !current.IsAdm;
+
+			var denyByContract =
+				isAuthenticated
+				&& !mandatory.HasFlag(AuthParams.IgnoreContract)
+				&& !access.Law.IsLastContractAccepted();
+
+			var denyByTFA =
+				isAuthenticated
+				&& !mandatory.HasFlag(AuthParams.IgnoreTFA)
+				&& !access.Auth.VerifyTicketTFA();
+
+			var denyByMobile =
+				isAuthenticated
+				&& mandatory.HasFlag(AuthParams.Mobile)
+				&& !access.Auth.VerifyTicketType(TicketType.Mobile);
+
+
+			if (!isAuthenticated)
+				makeResult(context, Error.LoginRequested);
+
+			else if (denyByTFA)
+				makeResult(context, Error.TFANotVerified);
+
+			else if (denyByContract)
+				makeResult(context, Error.NotSignedLastContract);
+
+			else if (denyByAdmin || denyByMobile)
+				makeResult(context, Error.Uninvited);
+		}
+
+		protected abstract void makeResult(
+			AuthorizationFilterContext context,
+			Error error
+		);
+	}
+}
