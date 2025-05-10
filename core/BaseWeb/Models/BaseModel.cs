f836a71be71a4d39bcbc95bfa0eccaa2487abfd1
Dara Keon <lc.sales.duarte@gmail.com>
2025-05-11 00:38:14 +0100

    core: move common class to base web

--- core/BaseWeb/Models/BaseModel.cs
+++ core/BaseWeb/Models/BaseModel.cs
@@ -1,23 +1,28 @@
 ﻿using System;
+using System.Collections.Generic;
 using DFM.BaseWeb.Extensions;
 using DFM.BaseWeb.Languages;
 using DFM.BaseWeb.Starters;
 using DFM.BusinessLogic;
 using DFM.BusinessLogic.Services;
 using DFM.Generic;
+using DFM.Logs;
 using Microsoft.AspNetCore.Http;
+using Service = DFM.BaseWeb.BusinessLogic.Service;
 
-namespace DFM.API.Models
+namespace DFM.BaseWeb.Models
 {
 	public abstract class BaseModel
 	{
-		public static Boolean IsDev { get; set; }
+		public static Boolean IsDev => Cfg.IsDev;
 
-		private static HttpContext context => Context.Accessor.HttpContext;
+		protected static HttpContext context => Context.Accessor.HttpContext;
 
 		protected Translator translator => context.GetTranslator();
+		protected Dictionary<String, String> route => context.GetRouteText();
 
-		private static ServiceAccess service => context.GetService().Access;
+		private Service contextService => context.GetService();
+		private ServiceAccess service => contextService.Access;
 
 		protected AuthService auth => service.Auth;
 		protected LawService law => service.Law;
@@ -28,15 +33,21 @@ namespace DFM.API.Models
 		protected OutsideService outside => service.Outside;
 		protected AttendantService attendant => service.Attendant;
 
+		protected ILogService logService => contextService.LogService;
+
 		protected Current current => service.Current;
 
 		protected DateTime now => current.Now;
 		protected Theme theme => current.Theme;
 		protected String language => translator.Language;
+		protected Boolean wizard => current.Wizard;
+
 
 		protected Boolean isUsingCategories => current.UseCategories;
 		protected Boolean isUsingAccountsSigns => current.UseAccountsSigns;
 		protected Boolean moveCheckingEnabled => current.MoveCheck;
+		protected Boolean isUsingCurrency => current.UseCurrency;
+
 		protected Int32 planLimitMoveDetail => current.PlanLimitMoveDetail;
 		protected Boolean tfaForgottenWarning => current.TFAForgottenWarning;
 
