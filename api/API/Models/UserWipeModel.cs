a9f6c2483fd068754baa7c4a1eca066924c1acae
Dara Keon <lc.sales.duarte@gmail.com>
2025-05-10 04:15:17 +0100

    chore: fix CLR types

--- api/API/Models/UserWipeModel.cs
+++ api/API/Models/UserWipeModel.cs
@@ -1,8 +1,10 @@
-﻿namespace DFM.API.Models
+﻿using System;
+
+namespace DFM.API.Models
 {
 	public class UserWipeModel : BaseApiModel
 	{
-		public string Password { get; set; }
+		public String Password { get; set; }
 
 		internal void AskWipe()
 		{
