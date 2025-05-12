f0faab2a78bff69e1fcad06b7c043a426fbbf270
Dara Keon <lc.sales.duarte@gmail.com>
2025-05-12 01:56:59 +0100

    core: fix repeated error codes

---
+++ core/Tests/BusinessLogic/Unit/Exceptions/ErrorTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using DFM.BusinessLogic.Exceptions;
+using DFM.Generic;
+using NUnit.Framework;
+
+namespace DFM.BusinessLogic.Tests.Unit.Exceptions;
+
+internal class ErrorTests
+{
+	[Test]
+	public void NonRepeatedValues()
+	{
+		var repeatedErrors = EnumX.AllValues<Error>()
+			.GroupBy(value => (Int32)value)
+			.Where(group => group.Count() > 1)
+			.ToList();
+
+		Assert.That(repeatedErrors, Is.Empty);
+	}
+}
