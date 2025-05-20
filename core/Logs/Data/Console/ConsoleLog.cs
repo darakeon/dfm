718abbc319d8096477689687709f7171ff7e0a80
Dara Keon <lc.sales.duarte@gmail.com>
2025-05-20 21:12:50 +0100

    robot: send logs to aws

---
+++ core/Logs/Data/Console/ConsoleLog.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace DFM.Logs.Data.Console;
+
+internal class ConsoleLog
+{
+	private ConsoleLog(Object message, ConsoleLogStatus status)
+	{
+		Status = status;
+		Message = message;
+	}
+
+	public ConsoleLogStatus Status { get; private set; }
+	public Object Message { get; private set; }
+
+	public static ConsoleLog Ok(Object message)
+	{
+		return new ConsoleLog(message, ConsoleLogStatus.Ok);
+	}
+
+	public static ConsoleLog Error(Object message)
+	{
+		return new ConsoleLog(message, ConsoleLogStatus.Error);
+	}
+
+	public override String ToString()
+	{
+		return $"[{Status}] {Message}";
+	}
+}
