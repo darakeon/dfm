a6de277843403f13747fe0e3f24b62995d424c3d
Dara Keon <lc.sales.duarte@gmail.com>
2025-05-20 21:12:50 +0100

    robot: send logs to aws

--- robot/Robot/Connection.cs
+++ robot/Robot/Connection.cs
@@ -12,24 +12,19 @@ namespace DFM.Robot
 {
 	class Connection
 	{
-		private const String envVarName = "ASPNETCORE_ENVIRONMENT";
 		private const String sessionName = "ROBOT";
-
-		private static readonly String name = 
-			Environment.GetEnvironmentVariable(envVarName);
 
 		public static async Task Run(Func<Task> action)
 		{
-			Cfg.Init(name);
-			PlainText.Initialize();
-
 			try
 			{
+				PlainText.Initialize();
+
 				await sessionFactory(() => session(action));
 			}
 			catch (Exception e)
 			{
-				LogFactory.Service.Log(e);
+				await LogFactory.Service.Log(e);
 				throw;
 			}
 		}
