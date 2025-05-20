1d823dedcc5af85b7bcad67bacae40b6d09fcea5
Dara Keon <lc.sales.duarte@gmail.com>
2025-05-20 21:12:50 +0100

    robot: send logs to aws

--- core/Logs/ILogService.cs
+++ core/Logs/ILogService.cs
@@ -10,5 +10,9 @@ public interface ILogService
 	Task LogHandled(Exception exception, String message);
 
 	Task LogApplication<T>(String category, LogLevel logLevel, EventId eventId, T state, Exception? exception, Func<T, Exception?, String> formatter);
+
 	Task LogRequest(String moment, String method, String path, DateTime time);
+
+	Task LogConsoleOk(object message);
+	Task LogConsoleError(object message);
 }
