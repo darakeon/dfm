321890cbe91c6ac76aefa6af9108133369e56a87
Dara Keon <lc.sales.duarte@gmail.com>
2025-06-04 00:25:04 +0100

    midna: send gunicorn logs to aws

--- midna/gunicorn/config/prod.py
+++ midna/gunicorn/config/prod.py
@@ -1,5 +1,27 @@
 """Gunicorn *production* config file"""
 
+from logging import getLogger, INFO, Formatter
+from traceback import format_exc
+
+from midna.cloudwatch import Boto3CloudWatchHandler
+
+
+error_logger = getLogger("gunicorn.error")
+error_logger.setLevel(INFO)
+
+access_logger = getLogger("gunicorn.access")
+access_logger.setLevel(INFO)
+
+try:
+    cloud_watch_handler = Boto3CloudWatchHandler()
+    cloud_watch_handler.setFormatter(Formatter(cloud_watch_handler.FORMAT))
+    error_logger.addHandler(cloud_watch_handler)
+    access_logger.addHandler(cloud_watch_handler)
+except Exception as e:
+    error_logger.warning(f"CloudWatch handler failed: {e}")
+    error_logger.warning(format_exc())
+
+
 # Django WSGI application path in pattern MODULE_NAME:VARIABLE_NAME
 wsgi_app = "midna.wsgi:application"
 
@@ -17,8 +39,8 @@ bind = "0.0.0.0:8627"
 reload = False
 
 # Write access and error info to /var/log
-accesslog = "/var/log/gunicorn/access.log"
-errorlog = "/var/log/gunicorn/error.log"
+accesslog = "-"
+errorlog = "-"
 
 # Redirect stdout/stderr to log file
 capture_output = True
