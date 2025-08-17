57854e9c7a83b4f90ca6d1dcf4382aea449a5192
Dara Keon <lc.sales.duarte@gmail.com>
2025-08-17 15:09:12 +0100

    firebase integration prototype

--- android/App/src/main/kotlin/com/darakeon/dfm/welcome/WelcomeActivity.kt
+++ android/App/src/main/kotlin/com/darakeon/dfm/welcome/WelcomeActivity.kt
@@ -1,7 +1,11 @@
 package com.darakeon.dfm.welcome
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import android.widget.Button
+import androidx.core.content.ContextCompat
 import com.darakeon.dfm.base.BaseActivity
 import com.darakeon.dfm.databinding.BottomMenuBinding
 import com.darakeon.dfm.databinding.WelcomeBinding
@@ -9,6 +13,8 @@ import com.darakeon.dfm.lib.extensions.applyGlyphicon
 import com.darakeon.dfm.lib.extensions.redirect
 import com.darakeon.dfm.login.LoginActivity
 import com.darakeon.dfm.moves.MovesService
+import com.google.firebase.messaging.FirebaseMessaging
+
 
 class WelcomeActivity : BaseActivity<WelcomeBinding>() {
 	override fun inflateBinding(): WelcomeBinding {
@@ -33,9 +39,45 @@ class WelcomeActivity : BaseActivity<WelcomeBinding>() {
 
 			MovesService.start(this)
 
+			FirebaseMessaging.getInstance().token.addOnCompleteListener { task ->
+				if (task.isSuccessful) {
+					val token = task.result
+					//println(token)
+				}
+			}
+
 			populateCache()
 		} else {
 			redirect<LoginActivity>()
+		}
+	}
+
+	private val requestPermissionLauncher = registerForActivityResult(
+		ActivityResultContracts.RequestPermission(),
+	) { isGranted: Boolean ->
+		if (isGranted) {
+			// FCM SDK (and your app) can post notifications.
+		} else {
+			// TODO: Inform user that that your app will not show notifications.
+		}
+	}
+
+	private fun askNotificationPermission() {
+		// This is only necessary for API level >= 33 (TIRAMISU)
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+			if (ContextCompat.checkSelfPermission(this, Manifest.permission.POST_NOTIFICATIONS) ==
+				PackageManager.PERMISSION_GRANTED
+			) {
+				// FCM SDK (and your app) can post notifications.
+			} else if (shouldShowRequestPermissionRationale(Manifest.permission.POST_NOTIFICATIONS)) {
+				// TODO: display an educational UI explaining to the user the features that will be enabled
+				//       by them granting the POST_NOTIFICATION permission. This UI should provide the user
+				//       "OK" and "No thanks" buttons. If the user selects "OK," directly request the permission.
+				//       If the user selects "No thanks," allow the user to continue without notifications.
+			} else {
+				// Directly ask for the permission
+				requestPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
+			}
 		}
 	}
 }
