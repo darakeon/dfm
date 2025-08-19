78b062800c10045ddd9cff6e76ac142263ec1712
Dara Keon <lc.sales.duarte@gmail.com>
2025-08-19 17:21:10 +0100

    android: add script to clean up metadata

---
+++ .circleci/android/verification_metadata_cleanup.py
@@ -0,0 +1,66 @@
+from os import path
+from re import search
+from xml.etree import ElementTree
+
+
+file_path = path.join(
+	path.dirname(__file__),
+	"..", "..", "android", "gradle", "verification-metadata.xml"
+)
+
+tree = ElementTree.parse(file_path)
+root = tree.getroot()
+
+tag_format = r"({.+}).+"
+tag_prefix = search(tag_format, root.tag).group(1)
+
+namespace = tag_prefix.replace("{", "").replace("}", "")
+ElementTree.register_namespace("", namespace)
+
+components = root.find(f"{tag_prefix}components")
+
+libs = {}
+
+for component in components:
+	group = component.attrib.get("group")
+	name = component.attrib.get("name")
+	version = component.attrib.get("version")
+
+	full_name = f"{group}:{name}"
+
+	if full_name not in libs or version > libs[full_name]:
+		libs[full_name] = version
+
+c = 0
+
+print("Before:", len(components))
+
+while c < len(components):
+	component = components[c]
+
+	group = component.attrib.get("group")
+	name = component.attrib.get("name")
+	version = component.attrib.get("version")
+
+	full_name = f"{group}:{name}"
+
+	if libs[full_name] != version:
+		components.remove(component)
+	else:
+		c += 1
+
+print("After:", len(components))
+
+
+tree.write(file_path, encoding="UTF-8", xml_declaration=True)
+
+
+with open(file_path, "r") as file:
+	content = file.readlines()
+
+content[0] = content[0].replace("'", '"')
+content = [line.replace(" />", "/>") for line in content]
+content[-1] += "\n"
+
+with open(file_path, "w") as file:
+	file.writelines(content)
