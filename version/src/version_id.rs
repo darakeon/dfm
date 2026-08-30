1b6da828c14886cb9ac86798a205f08108bd42e7
Dara Keon <lc.sales.duarte@gmail.com>
2026-08-30 21:13:47 +0100

    version: refactor version files
    Given there is a file to handle version per se, no need to create a
    second one to save the file

--- version/src/version_id.rs
+++
@@ -1,8 +0,0 @@
-use crate::file::{get_path, set_content};
-use crate::version::Version;
-
-fn path() -> String { get_path(vec!["..", "docs", "current-version"]) }
-
-pub fn update_version_id(version: &Version) {
-	set_content(path(), version.dev.clone());
-}
